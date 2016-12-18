e2c83899051d7c2fd0a3607d87a395e6e9b709d4
Alexandre Hamez <alexandre.hamez@gmail.com>
2016-12-18 09:36:02 +0100

    Cleanup guards in Util

--- lib/protox/util.ex
+++ lib/protox/util.ex
@@ -1,50 +1,45 @@
 defmodule Protox.Util do
 
-  defmacro is_primitive(type) do
-    quote do
-      unquote(type) == :int32 or unquote(type) == :int64 or\
-      unquote(type) == :uint32 or unquote(type) == :uint64 or\
-      unquote(type) == :sint32 or unquote(type) == :sint64 or\
-      unquote(type) == :bool or unquote(type) == :float or\
-      unquote(type) == :fixed32 or unquote(type) == :sfixed32 or\
-      unquote(type) == :double or unquote(type) == :fixed64 or\
-      unquote(type) == :sfixed64
 
-    end
+  @primitive_fixed32 [
+    :fixed32, :sfixed32, :float,
+  ]
+
+  @primitive_fixed64 [
+    :fixed64, :sfixed64, :double,
+  ]
+
+  @primitive_fixed @primitive_fixed32 ++ @primitive_fixed64
+
+
+  @primitive_varint [
+    :int32, :uint32, :sint32, :int64, :uint64, :sint64, :bool
+  ]
+
+  @primitives @primitive_varint ++ @primitive_fixed
+
+  defmacro is_primitive(type) do
+    quote do: unquote(type) in unquote(@primitives)
   end
 
 
   defmacro is_primitive_varint(type) do
-    quote do
-      unquote(type) == :int32 or unquote(type) == :int64 or\
-      unquote(type) == :uint32 or unquote(type) == :uint64 or\
-      unquote(type) == :sint32 or unquote(type) == :sint64 or\
-      unquote(type) == :bool or unquote(type)
-    end
+    quote do: unquote(type) in unquote(@primitive_varint)
   end
 
 
   defmacro is_primitive_fixed32(type) do
-    quote do
-      unquote(type) == :float or unquote(type) == :fixed32 or unquote(type) == :sfixed32
-    end
+    quote do: unquote(type) in unquote(@primitive_fixed32)
   end
 
 
   defmacro is_primitive_fixed64(type) do
-    quote do
-      unquote(type) == :fixed64 or unquote(type) == :sfixed64 or unquote(type) == :double
-    end
+    quote do: unquote(type) in unquote(@primitive_fixed64)
   end
 
 
   defmacro is_primitive_fixed(type) do
-    quote do
-      # TODO. How can we factorize this?
-      unquote(type) == :fixed64 or unquote(type) == :sfixed64 or\
-      unquote(type) == :double or unquote(type) == :float or\
-      unquote(type) == :fixed32 or unquote(type) == :sfixed32
-    end
+    quote do: unquote(type) in unquote(@primitive_fixed)
   end
 
 
