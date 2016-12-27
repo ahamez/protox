efb40f05e33005f8cc9b14c0a2ea93272ad1b912
Alexandre Hamez <alexandre.hamez@gmail.com>
2016-12-27 18:50:33 +0100

    More tests

--- test/protox/parse_test.exs
+++ test/protox/parse_test.exs
@@ -33,6 +33,7 @@ defmodule Protox.ParseTest do
     assert field(fs, 7) == {:optional, :oneof_1_proto2a, {:oneof, :oneof_1}, {:message, Proto2A}}
     assert field(fs, 8) == {nil, :map_string_timestamp, :map, {:string, {:message, Google.Protobuf.Timestamp}}}
     assert field(fs, 9) == {:optional, :bytes, {:default, <<>>}, :bytes}
+    assert field(fs, 10) == {nil, :map_int64_nested_enum, :map, {:int64, {:enum, Abc.Def.Proto3.NestedEnum}}}
     assert field(fs, 134) == {:optional, :oneof_2_int32, {:oneof, :oneof_2}, :int32}
     assert field(fs, 135) == {:optional, :oneof_2_nested_enum, {:oneof, :oneof_2}, {:enum, Abc.Def.Proto3.NestedEnum}}
     assert field(fs, 9999) == {:optional, :nested_enum, {:default, :FOO}, {:enum, Abc.Def.Proto3.NestedEnum}}
@@ -64,6 +65,7 @@ defmodule Protox.ParseTest do
     assert field(fs, 3) == {:optional, :optional_nested_message, {:default, nil}, {:message, Proto2A.NestedMessage}}
     assert field(fs, 4) == {:repeated, :repeated_nested_enum, :unpacked, {:enum, Proto2A.NestedEnum}}
     assert field(fs, 5) == {:repeated, :repeated_nested_message, :unpacked, {:message, Proto2A.NestedMessage}}
+    assert field(fs, 6) == {:optional, :bytes, {:default, <<96, 118>>}, :bytes}
     assert field(fs, 126) == {:optional, :extension_int32, {:default, nil}, :int32}
     assert field(fs, 199) == {:optional, :extension_double, {:default, 42.42}, :double}
 
