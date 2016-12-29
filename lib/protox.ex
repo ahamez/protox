2989097de84ff9d27a133bfe1dacc9c30baad4de
Alexandre Hamez <alexandre.hamez@gmail.com>
2016-12-29 12:06:23 +0100

    It's now possible to generate modules from .proto files

---
+++ lib/protox.ex
@@ -0,0 +1,61 @@
+defmodule Protox do
+
+  @moduledoc"""
+  Use this module to generate the Elixir modules from a set of protobuf definitions:
+
+    ```
+    defmodule Foo do
+      use Protox, files: [
+        "./defs/foo.proto",
+        "./defs/bar.proto",
+        "./defs/baz/fiz.proto",
+      ]
+    end
+    ```
+
+  Note that the files should reside in the same sub-directory.
+
+  It's also possible to give a definition as a struct:
+
+    ```
+    defmodule Bar do
+      use Protox, \"\"\"
+        syntax = "proto3";
+        package fiz;
+
+        message Baz {
+        }
+
+        message Foo {
+          map<int32, Baz> b = 2;
+        }
+      \"\"\"
+    end
+    ```
+
+  The generated modules respect the package declaration. For instance, in the above example,
+  both the `Fiz.Baz` and `Fiz.Foo` modules will be generated.
+
+  """
+
+  defmacro __using__(opts) do
+
+    files = case opts do
+      text when is_binary(text) ->
+        filename = "#{__CALLER__.module}_#{:crypto.hash(:sha, text) |> Base.encode16()}.proto"
+        filepath = Path.join([Mix.Project.build_path(), filename]) |> Path.expand()
+        File.write!(filepath, text)
+        [filepath]
+
+      files: files ->
+        files |> Enum.map(&Path.expand/1)
+    end
+
+    {:ok, file_descriptor_set} = Protox.Protoc.run(files)
+    {enums, messages} = Protox.Parse.parse(file_descriptor_set)
+
+    Protox.Define.define(enums, messages)
+  end
+
+end
+
--- lib/protox/protoc.ex
+++ lib/protox/protoc.ex
@@ -2,18 +2,46 @@ defmodule Protox.Protoc do
 
   @moduledoc false
 
-  def run(proto_files) do
-    filename = Protox.Util.random_string()
-    file = "#{Mix.Project.build_path}/protox_#{filename}"
-    ret = System.cmd(
-      "protoc",
-      ["--include_imports", "-o", file] ++ proto_files
-    )
+  def run([proto_file]) do
+    do_run([proto_file], ["-I", "#{proto_file |> Path.dirname() |> Path.expand()}"])
+  end
 
-    case ret do
-      {_, 0}   -> {:ok, File.read!(file)}
+
+  def run(proto_files) do
+    do_run(proto_files, ["-I", "#{common_directory_path(proto_files)}"])
+  end
+
+
+  # -- Private
+
+
+  defp do_run(proto_files, args) do
+    outfile_name = "protox_#{Protox.Util.random_string()}"
+    outfile_path = Path.join([Mix.Project.build_path(), outfile_name])
+    cmd_args = ["--include_imports", "-o", outfile_path] ++ args ++ proto_files
+
+    ret = case System.cmd("protoc", cmd_args) do
+      {_, 0}   -> {:ok, File.read!(outfile_path)}
       {msg, _} -> {:error, msg}
     end
+
+    :ok = File.rm(outfile_path)
+
+    ret
+  end
+
+
+  defp common_directory_path(paths_rel) do
+    paths = Enum.map(paths_rel, &Path.expand/1)
+
+    min_path = paths |> Enum.min() |> Path.split()
+    max_path = paths |> Enum.max() |> Path.split()
+
+    min_path
+    |> Enum.zip(max_path)
+    |> Enum.take_while(fn {a, b} -> a == b end)
+    |> Enum.map(fn {x, _} -> x end)
+    |> Path.join()
   end
 
 end
