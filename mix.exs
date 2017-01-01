56453b02a9a29efd38041184f22d4622b8684872
Alexandre Hamez <alexandre.hamez@gmail.com>
2017-01-01 16:22:20 +0100

    Prep for hex.pm

--- mix.exs
+++ mix.exs
@@ -2,18 +2,24 @@ defmodule Protox.Mixfile do
 
   use Mix.Project
 
+
   def project do
     [
       app: :protox,
-      version: "0.1.0",
+      version: "0.7.0",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      escript: [main_module: Protox.Escript.Main]
+      escript: [main_module: Protox.Escript.Main],
+      name: "Protox",
+      source_url: "https://github.com/ahamez/protox",
+      description: description(),
+      package: package(),
     ]
   end
+
 
   def application do
     [
@@ -21,14 +27,33 @@ defmodule Protox.Mixfile do
     ]
   end
 
+
   defp deps do
     [
-      {:credo, "~> 0.5"},
+      {:credo, "~> 0.5", only: [:dev]},
       {:dialyxir, "~> 0.4", only: [:dev], runtime: false},
-      {:ex_doc, "~> 0.14"},
+      {:ex_doc, "~> 0.14", only: [:dev]},
       {:excoveralls, "~> 0.5", only: :test},
       {:inch_ex,  "~> 0.5.5", only: :docs},
       {:varint, "~> 1.1.0"},
+    ]
+  end
+
+
+  defp description do
+    """
+    A library for Protocol Buffers
+    """
+  end
+
+
+  defp package do
+    [
+      name: :protox,
+      files: ["lib", "mix.exs", "README*", "LICENSE"],
+      maintainers: ["Alexandre Hamez"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/ahamez/protox",}
     ]
   end
 
