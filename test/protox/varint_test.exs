42b37a2fa9844dea5d1e975de32d2cf29de605d2
Alexandre Hamez <alexandre.hamez@gmail.com>
2017-01-29 14:00:48 +0100

    Test over-encoded varints
    Seen in https://github.com/google/protobuf/commit/ffa71f80070fde3eb8e6a5779df951c4fe0729e0?diff=unified#diff-232536caf41e6af73e06789c36447fde

--- test/protox/varint_test.exs
+++ test/protox/varint_test.exs
@@ -10,10 +10,12 @@ defmodule Protox.VarintTest do
 
 
   test "Decode" do
-    assert Protox.Varint.decode(<<172, 2>>)    == {300, <<>>}
-    assert Protox.Varint.decode(<<172, 2, 0>>) == {300, <<0>>}
-    assert Protox.Varint.decode(<<0>>)         == {0  , <<>>}
-    assert Protox.Varint.decode(<<1>>)         == {1  , <<>>}
+    assert Protox.Varint.decode(<<172, 2>>)        == {300  , <<>>}
+    assert Protox.Varint.decode(<<172, 2, 0>>)     == {300  , <<0>>}
+    assert Protox.Varint.decode(<<0>>)             == {0    , <<>>}
+    assert Protox.Varint.decode(<<1>>)             == {1    , <<>>}
+    assert Protox.Varint.decode(<<185, 96>>)       == {12345, <<>>}
+    assert Protox.Varint.decode(<<185, 224, 0>>)   == {12345, <<>>}
   end
 
 end
