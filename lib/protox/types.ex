daf1f7230946d8c68f8bbbc099e5ff9b6c1fbeb2
Alexandre Hamez <alexandre.hamez@gmail.com>
2017-01-29 09:42:44 +0100

    Reify map key type

--- lib/protox/types.ex
+++ lib/protox/types.ex
@@ -1,12 +1,14 @@
 defmodule Protox.Types do
   @moduledoc false
 
-  @type tag  :: 0 | 1 | 2 | 5
+  @type tag :: 0 | 1 | 2 | 5
   @type kind :: {:default, any} | :packed | :unpacked | :map | {:oneof, atom}
-  @type type ::   :fixed32 | :sfixed32 | :float | :fixed64 | :sfixed64 | :double
-                | :int32 | :uint32 | :sint32 | :int64 | :uint64 | :sint64 |:bool
-                | :string | :bytes
-                | {:enum, atom} | {:message, atom}
-                | {type, type} # map
+  @type map_key_type :: :int32 | :int64 | :uint32 | :uint64 | :sint32 | :sint64 | :fixed32
+                    | :fixed64 | :sfixed32 | :sfixed64 | :bool | :string
+  @type type :: :fixed32 | :sfixed32 | :float | :fixed64 | :sfixed64 | :double
+            | :int32 | :uint32 | :sint32 | :int64 | :uint64 | :sint64 |:bool
+            | :string | :bytes
+            | {:enum, atom} | {:message, atom}
+            | {map_key_type , type} # map
 
 end
