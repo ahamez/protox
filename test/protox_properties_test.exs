32b34173307785ae55d8746ef42bd6f4875b7163
Alexandre Hamez <alexandre.hamez@gmail.com>
2020-05-27 22:43:37 +0200

    Remove verbose tag for property

--- test/protox_properties_test.exs
+++ test/protox_properties_test.exs
@@ -2,7 +2,7 @@ defmodule Protox.PropertiesTest do
   use ExUnit.Case
   use PropCheck
 
-  property "Upper", [:verbose] do
+  property "Upper" do
     forall {msg, encoded, encoded_bin, decoded} <- generate(Upper) do
       is_list(encoded) and is_binary(encoded_bin) and decoded == msg
     end
