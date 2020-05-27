1fd96eb6d8f4301c3d38697fbd4f7aaddb5b71ed
Alexandre Hamez <alexandre.hamez@gmail.com>
2020-05-27 22:43:14 +0200

    Properties to test zigzag encoding

--- test/protox/zigzag_test.exs
+++ test/protox/zigzag_test.exs
@@ -1,7 +1,8 @@
 defmodule Protox.ZigzagTest do
   use ExUnit.Case
+  use PropCheck
 
-  test "Encode" do
+  test "Zigzag encode" do
     assert Protox.Zigzag.encode(0) == 0
     assert Protox.Zigzag.encode(-1) == 1
     assert Protox.Zigzag.encode(1) == 2
@@ -10,12 +11,25 @@ defmodule Protox.ZigzagTest do
     assert Protox.Zigzag.encode(-2_147_483_648) == 4_294_967_295
   end
 
-  test "Decode" do
+  property "Zigzag encode" do
+    forall value <- integer() do
+      Protox.Zigzag.encode(value) >= 0
+    end
+  end
+
+  test "Zigzag decode" do
     assert Protox.Zigzag.decode(0) == 0
     assert Protox.Zigzag.decode(1) == -1
     assert Protox.Zigzag.decode(2) == 1
     assert Protox.Zigzag.decode(3) == -2
     assert Protox.Zigzag.decode(4_294_967_294) == 2_147_483_647
     assert Protox.Zigzag.decode(4_294_967_295) == -2_147_483_648
+  end
+
+  property "Zigzag decode" do
+    forall value <- range(0, 4_294_967_295) do
+      decoded = Protox.Zigzag.decode(value)
+      decoded <= 2_147_483_647 and decoded >= -2_147_483_648
+    end
   end
 end
