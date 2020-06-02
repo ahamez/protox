b75d8431f931836ee0d7d831e05519c10982d3ec
Alexandre Hamez <alexandre.hamez@gmail.com>
2020-06-02 06:39:00 +0200

    Elixir 1.6 as minimal required version to avoid warnings
    warning: the dependency :erlex requires Elixir "~> 1.6" but you are running on v1.4.5
    warning: the dependency :propcheck requires Elixir "~> 1.5" but you are running on v1.4.5
    warning: the dependency :dialyxir requires Elixir ">= 1.6.0" but you are running on v1.4.5

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Protox.Mixfile do
     [
       app: :protox,
       version: "0.20.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps(),
