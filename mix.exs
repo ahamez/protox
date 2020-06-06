1e91473e0e9e574f66db445ec4825e4b488441e2
Alexandre Hamez <alexandre.hamez@gmail.com>
2020-06-06 17:42:08 +0200

    0.22.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Protox.Mixfile do
   def project do
     [
       app: :protox,
-      version: "0.21.0",
+      version: "0.22.0",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
