5a70cb47214315f22b67dc89def5a3dc64d6fabf
Alexandre Hamez <alexandre.hamez@gmail.com>
2020-12-10 14:33:16 +0100

    Make sure that conformance tests are really launched.
    We were previously relying on a manual check of the tests output.
    Also, the output is now much less verbose as the conformance test runner is launched with Mix.Shell.Quiet.cmd/1.

--- conformance/mix/tasks/protox/task.ex
+++ conformance/mix/tasks/protox/task.ex
@@ -9,10 +9,22 @@ defmodule Mix.Tasks.Protox.Conformance do
     with {options, _, []} <- OptionParser.parse(args, strict: [runner: :string]),
          {:ok, runner} <- Keyword.fetch(options, :runner),
          :ok <- Mix.Tasks.Escript.Build.run([]),
-         0 <- Mix.shell().cmd("#{runner} --enforce_recommended ./protox_conformance") do
-      :ok
+         :ok <- launch(runner) do
+      {:ok, :conformance_successful}
     else
-      _ -> :error
+      #
+      {:error, :runner_failure} -> {:ok, :conformance_failure}
+      e -> e
+    end
+  end
+
+  defp launch(runner) do
+    case Mix.Shell.Quiet.cmd("#{runner} --enforce_recommended ./protox_conformance") do
+      0 -> :ok
+      1 -> {:error, :runner_failure}
+      126 -> {:error, :cannot_execute_runner}
+      127 -> {:error, :no_such_file_or_directory}
+      code -> {:error, code}
     end
   end
 end
--- test/protox_test.exs
+++ test/protox_test.exs
@@ -326,11 +326,16 @@ defmodule ProtoxTest do
     runner = System.get_env("PROTOBUF_CONFORMANCE_RUNNER")
     assert runner != nil, "PROTOBUF_CONFORMANCE_RUNNER not set"
 
-    Mix.Tasks.Protox.Conformance.run(["--runner=#{runner}"])
+    assert File.exists?(runner)
+
+    # {:ok, _} here just means that the runner could be launched, not that the conformance
+    # test performed correctly. We'll check the absence of the "failing_tests.txt" file
+    # to verify this.
+    assert {:ok, _} = Mix.Tasks.Protox.Conformance.run(["--runner=#{runner}"])
 
     # protobuf conformance runner produces this file only when some tests have failed
     refute File.exists?("./failing_tests.txt"),
-           "Please check 'failing_tests.txt' file and 'conformance' directory for more information about this failure"
+           "Please check 'failing_tests.txt' file and 'conformance_report' directory for more information about this failure"
   end
 
   defp reencode_with_protoc(encoded, mod) do
