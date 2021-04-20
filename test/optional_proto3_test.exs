43becb5ee1560f3e74c8352c8a683cdbb5f0276a
Alexandre Hamez <alexandre.hamez@gmail.com>
2021-04-20 23:19:39 +0200

    More tests for optional proto3 fields

---
+++ test/optional_proto3_test.exs
@@ -0,0 +1,81 @@
+defmodule OptionalProto3Test do
+  use ExUnit.Case
+
+  use Protox,
+    schema: """
+      syntax = "proto3";
+
+      message Msg1 {
+        optional int32 foo = 1;
+      }
+
+      message Msg2 {
+        oneof _foo {
+          int32 foo = 1;
+        }
+      }
+
+      message Msg3 {
+        optional Msg1 foo = 1;
+      }
+
+      message Msg4 {
+        oneof _foo {
+          Msg1 foo = 1;
+        }
+      }
+    """
+
+  test "An proto3 optional field is encoded as a oneof" do
+    msg1 = %Msg1{foo: 1}
+    msg2 = %Msg2{_foo: {:foo, 1}}
+
+    encoded_msg1 = msg1 |> Msg1.encode!() |> :binary.list_to_bin()
+    encoded_msg2 = msg2 |> Msg2.encode!() |> :binary.list_to_bin()
+
+    assert encoded_msg1 == encoded_msg2
+  end
+
+  test "An proto3 synthetic oneof can be decoded as an optional field" do
+    msg1 = %Msg1{foo: 1}
+    msg2 = %Msg2{_foo: {:foo, 1}}
+
+    assert msg2 |> Msg2.encode!() |> :binary.list_to_bin() |> Msg1.decode!() == msg1
+  end
+
+  test "An unset proto3 optional field is not serialized" do
+    explicit_nil = %Msg1{foo: nil}
+    implicit_nil = %Msg1{}
+
+    assert explicit_nil |> Msg1.encode!() |> :binary.list_to_bin() == <<>>
+    assert implicit_nil |> Msg1.encode!() |> :binary.list_to_bin() == <<>>
+  end
+
+  test "An proto3 optional empty message field is encoded as a oneof" do
+    msg3 = %Msg3{foo: %Msg1{}}
+    msg4 = %Msg4{_foo: {:foo, %Msg1{}}}
+
+    encoded_msg3 = msg3 |> Msg3.encode!() |> :binary.list_to_bin()
+    encoded_msg4 = msg4 |> Msg4.encode!() |> :binary.list_to_bin()
+
+    assert encoded_msg3 == encoded_msg4
+  end
+
+  test "An proto3 optional non-empty message field is encoded as a oneof" do
+    msg3 = %Msg3{foo: %Msg1{foo: -42}}
+    msg4 = %Msg4{_foo: {:foo, %Msg1{foo: -42}}}
+
+    encoded_msg3 = msg3 |> Msg3.encode!() |> :binary.list_to_bin()
+    encoded_msg4 = msg4 |> Msg4.encode!() |> :binary.list_to_bin()
+
+    assert encoded_msg3 == encoded_msg4
+  end
+
+  test "An unset proto3 optional message field is not serialized" do
+    explicit_nil = %Msg3{foo: nil}
+    implicit_nil = %Msg3{}
+
+    assert explicit_nil |> Msg3.encode!() |> :binary.list_to_bin() == <<>>
+    assert implicit_nil |> Msg3.encode!() |> :binary.list_to_bin() == <<>>
+  end
+end
