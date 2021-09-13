e43419a858c736c57a5f0b46cbb85ecfc7e2af6e
Alexandre Hamez <alexandre.hamez@gmail.com>
2021-09-13 19:21:05 +0200

    Deprecate defs()/0 and defs_by_name()/0

--- lib/protox/define_message.ex
+++ lib/protox/define_message.ex
@@ -32,11 +32,13 @@ defmodule Protox.DefineMessage do
           unquote(encoder)
           unquote(decoder)
 
+          @deprecated "Use fields/0 instead"
           @spec defs() :: %{
                   required(non_neg_integer) => {atom, Protox.Types.kind(), Protox.Types.type()}
                 }
           def defs(), do: unquote(fields_map)
 
+          @deprecated "Use fields/0 instead"
           @spec defs_by_name() :: %{
                   required(atom) => {non_neg_integer, Protox.Types.kind(), Protox.Types.type()}
                 }
