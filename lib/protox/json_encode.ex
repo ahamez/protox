5448ba5bbe04ce9925c7073b8409bc53c2c7ed15
Alexandre Hamez <alexandre.hamez@gmail.com>
2021-09-13 19:32:30 +0200

    Use new fields/0 function in JSON encoder

--- lib/protox/json_encode.ex
+++ lib/protox/json_encode.ex
@@ -3,6 +3,8 @@ defmodule Protox.JsonEncode do
   TODO
   """
 
+  alias Protox.Field
+
   use Protox.Float
 
   @spec encode!(struct()) :: iodata()
@@ -10,9 +12,8 @@ defmodule Protox.JsonEncode do
     initial_acc = ["}"]
 
     body =
-      msg.__struct__.defs()
-      |> Map.to_list()
-      |> Enum.reduce(initial_acc, fn field, acc ->
+      msg.__struct__.fields()
+      |> Enum.reduce(initial_acc, fn %Field{} = field, acc ->
         case encode_msg_field(msg, field, json_encoder) do
           <<>> ->
             acc
@@ -28,13 +29,13 @@ defmodule Protox.JsonEncode do
     ["{" | body]
   end
 
-  defp encode_msg_field(_msg, {_tag, {_name, {:oneof, _parent}, _type}}, _json_encoder) do
+  defp encode_msg_field(_msg, %Field{kind: {:oneof, _parent}}, _json_encoder) do
     <<>>
   end
 
-  defp encode_msg_field(msg, {_tag, {name, kind, type}}, json_encoder) do
-    field_value = Map.fetch!(msg, name)
-    json_value = encode_field(field_value, kind, type, json_encoder)
+  defp encode_msg_field(msg, %Field{} = field, json_encoder) do
+    field_value = Map.fetch!(msg, field.name)
+    json_value = encode_field(field, field_value, json_encoder)
 
     case json_value do
       <<>> ->
@@ -42,7 +43,7 @@ defmodule Protox.JsonEncode do
 
       _ ->
         json_name =
-          name
+          field.name
           |> Atom.to_string()
           |> lower_camel_case()
           |> json_encoder.encode!()
@@ -51,54 +52,87 @@ defmodule Protox.JsonEncode do
     end
   end
 
-  defp encode_field(nil, {:default, _}, {:message, _msg_type}, _json_encoder) do
+  defp encode_field(%Field{kind: {:default, _}, type: {:message, _msg}}, nil, _json_encoder) do
     <<>>
   end
 
-  defp encode_field(value, {:default, _default_value}, {:message, _msg_type}, json_encoder) do
+  defp encode_field(
+         %Field{kind: {:default, _default_value}, type: {:message, _msg}},
+         value,
+         json_encoder
+       ) do
     encode!(value, json_encoder)
   end
 
-  defp encode_field(value, {:default, default_value}, {:enum, _msg_type}, _json_encoder)
+  defp encode_field(
+         %Field{kind: {:default, default_value}, type: {:enum, _enum}},
+         value,
+         _json_encoder
+       )
        when value == default_value do
     <<>>
   end
 
-  defp encode_field(value, {:default, _default_value}, {:enum, enum_type}, json_encoder)
+  defp encode_field(
+         %Field{kind: {:default, _default_value}, type: {:enum, enum}},
+         value,
+         json_encoder
+       )
        when is_integer(value) do
-    value |> enum_type.decode() |> json_encoder.encode!()
+    value |> enum.decode() |> json_encoder.encode!()
   end
 
-  defp encode_field(value, {:default, _default_value}, {:enum, _msg_type}, json_encoder)
+  defp encode_field(
+         %Field{kind: {:default, _default_value}, type: {:enum, _enum}},
+         value,
+         json_encoder
+       )
        when is_atom(value) do
     json_encoder.encode!(value)
   end
 
-  defp encode_field(<<>>, {:default, _default_value}, :bytes, _json_encoder) do
+  defp encode_field(%Field{kind: {:default, _default_value}, type: :bytes}, <<>>, _json_encoder) do
     <<>>
   end
 
-  defp encode_field(value, {:default, _default_value}, :bytes, json_encoder) do
+  defp encode_field(%Field{kind: {:default, _default_value}, type: :bytes}, value, json_encoder) do
     value |> Base.encode64() |> json_encoder.encode!()
   end
 
-  defp encode_field(value, {:default, default_value}, _type, _json_encoder)
+  defp encode_field(%Field{kind: {:default, default_value}}, value, _json_encoder)
        when value == default_value do
     <<>>
   end
 
-  defp encode_field(@positive_infinity_32, _kind, :float, _json_encoder), do: "\"Infinity\""
-  defp encode_field(@negative_infinity_32, _kind, :float, _json_encoder), do: "\"-Infinity\""
-  defp encode_field(@nan_32, _kind, :float, _json_encoder), do: "\"NaN\""
-  defp encode_field(@positive_infinity_64, _kind, :double, _json_encoder), do: "\"Infinity\""
-  defp encode_field(@negative_infinity_64, _kind, :double, _json_encoder), do: "\"-Infinity\""
-  defp encode_field(@nan_64, _kind, :double, _json_encoder), do: "\"NaN\""
+  defp encode_field(%Field{type: :float}, @positive_infinity_32, _json_encoder) do
+    "\"Infinity\""
+  end
 
-  defp encode_field(value, {:default, _default_value}, _type, json_encoder) do
+  defp encode_field(%Field{type: :float}, @negative_infinity_32, _json_encoder) do
+    "\"-Infinity\""
+  end
+
+  defp encode_field(%Field{type: :float}, @nan_32, _json_encoder) do
+    "\"NaN\""
+  end
+
+  defp encode_field(%Field{type: :double}, @positive_infinity_64, _json_encoder) do
+    "\"Infinity\""
+  end
+
+  defp encode_field(%Field{type: :double}, @negative_infinity_64, _json_encoder) do
+    "\"-Infinity\""
+  end
+
+  defp encode_field(%Field{type: :double}, @nan_64, _json_encoder) do
+    "\"NaN\""
+  end
+
+  defp encode_field(%Field{kind: {:default, _default_value}}, value, json_encoder) do
     json_encoder.encode!(value)
   end
 
-  defp encode_field(_value, _kind, _type, _json_encoder) do
+  defp encode_field(_field, _type, _json_encoder) do
     <<>>
   end
 
