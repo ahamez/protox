73a9841797574844b175b3e86f463226b2a78499
Alexandre Hamez <alexandre.hamez@gmail.com>
2021-09-14 22:23:54 +0200

    Add JSON encoding of Google.Protobuf.FieldMask

--- lib/protox/json_message_encoder.ex
+++ lib/protox/json_message_encoder.ex
@@ -55,3 +55,66 @@ defimpl Protox.JsonMessageEncoder, for: Google.Protobuf.Timestamp do
     end
   end
 end
+
+defimpl Protox.JsonMessageEncoder, for: Google.Protobuf.FieldMask do
+  def encode_message(msg, _json_encode) do
+    case check_paths(msg.paths) do
+      :ok ->
+        msg.paths
+        |> Enum.map(&lower_camel_case/1)
+        |> Enum.join(",")
+
+      :error ->
+        raise Protox.JsonEncodingError.new(msg, "path is invalid")
+    end
+  end
+
+  defp lower_camel_case(string) do
+    string
+    |> String.split(".")
+    |> Enum.map(fn str ->
+      <<first, rest::binary>> = Macro.camelize(str)
+
+      <<String.downcase(<<first>>, :ascii)::binary, rest::binary>>
+    end)
+    |> Enum.join(".")
+  end
+
+  defp check_paths(paths) do
+    res =
+      Enum.any?(paths, fn path ->
+        with false <- has_too_many_underscores?(path),
+             false <- has_a_number_in_path_component?(path),
+             false <- has_camel_cased_components?(path) do
+          false
+        else
+          _ ->
+            true
+        end
+      end)
+
+    if res do
+      :error
+    else
+      :ok
+    end
+  end
+
+  defp has_too_many_underscores?(path) do
+    String.contains?(path, "__")
+  end
+
+  defp has_a_number_in_path_component?(path) do
+    path
+    |> String.split("_")
+    |> Enum.any?(fn str ->
+      String.starts_with?(str, ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])
+    end)
+  end
+
+  defp has_camel_cased_components?(path) do
+    path
+    |> String.split("_")
+    |> Enum.any?(fn str -> str == lower_camel_case(str) end)
+  end
+end
