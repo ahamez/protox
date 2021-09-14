af321a3a7d8c15c121601b24257ef36bd56da7c7
Alexandre Hamez <alexandre.hamez@gmail.com>
2021-09-14 21:22:33 +0200

    Add JSON encoding of Google.Protobuf.Timestamp

--- lib/protox/json_message_encoder.ex
+++ lib/protox/json_message_encoder.ex
@@ -36,3 +36,22 @@ defimpl Protox.JsonMessageEncoder, for: Google.Protobuf.Duration do
     end
   end
 end
+
+defimpl Protox.JsonMessageEncoder, for: Google.Protobuf.Timestamp do
+  def encode_message(msg, _json_encode) do
+    unix_timestamp = msg.seconds * 1_000_000_000 + msg.nanos
+
+    cond do
+      unix_timestamp > 253_402_300_799 * 1_000_000_000 ->
+        raise Protox.JsonEncodingError.new(msg, "timestamp is > 9999-12-31T23:59:59.999999999Z")
+
+      unix_timestamp < -62_135_596_800 * 1_000_000_000 ->
+        raise Protox.JsonEncodingError.new(msg, "timestamp is < 0001-01-01T00:00:00Z")
+
+      true ->
+        unix_timestamp
+        |> DateTime.from_unix!(:nanosecond)
+        |> DateTime.to_iso8601()
+    end
+  end
+end
