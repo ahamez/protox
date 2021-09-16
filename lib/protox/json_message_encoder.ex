1b5509e9b9ea32f9a5e7b34290970a2067432029
Alexandre Hamez <alexandre.hamez@gmail.com>
2021-09-16 22:14:31 +0200

    Comments

--- lib/protox/json_message_encoder.ex
+++ lib/protox/json_message_encoder.ex
@@ -59,9 +59,11 @@ defimpl Protox.JsonMessageEncoder, for: Google.Protobuf.Timestamp do
     unix_timestamp = msg.seconds * 1_000_000_000 + msg.nanos
 
     cond do
+      # 9999-12-31T23:59:59.999999999Z as UNIX date in nanoseconds
       unix_timestamp > 253_402_300_799_999_999_000 ->
         raise Protox.JsonEncodingError.new(msg, "timestamp is > 9999-12-31T23:59:59.999999999Z")
 
+      # 0001-01-01T00:00:00Z as UNIX date in nanoseconds
       unix_timestamp < -62_135_596_800_000_000_000 ->
         raise Protox.JsonEncodingError.new(msg, "timestamp is < 0001-01-01T00:00:00Z")
 
