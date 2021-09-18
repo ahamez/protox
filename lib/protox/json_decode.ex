24d357934c55d1f98b5cc13d4f6bb86d10563abb
Alexandre Hamez <alexandre.hamez@gmail.com>
2021-09-18 21:08:32 +0200

    Support JSON decoding of all scalar types

--- lib/protox/json_decode.ex
+++ lib/protox/json_decode.ex
@@ -3,7 +3,7 @@ defmodule Protox.JsonDecode do
 
   alias Protox.Field
 
-  # @spec decode!(atom(), iodata(), fun()) :: struct() | no_return()
+  # @spec decode!(iodata(), atom(), fun()) :: struct() | no_return()
   def decode!(input, mod, json_decode) do
     json = json_decode.(input)
 
@@ -33,6 +33,54 @@ defmodule Protox.JsonDecode do
 
   defp decode_msg_field(%Field{} = _field, _json_value) do
     raise "TODO"
+  end
+
+  defp decode_value(json_value, type)
+       when is_binary(json_value) and type in [:int64, :fixed64, :sfixed64, :uint64] do
+    String.to_integer(json_value)
+  end
+
+  defp decode_value("Infinity", type) when type in [:double, :float], do: :infinity
+  defp decode_value("-Infinity", type) when type in [:double, :float], do: :"-infinity"
+  defp decode_value("NaN", type) when type in [:double, :float], do: :nan
+
+  defp decode_value(json_value, type) when is_binary(json_value) and type in [:double, :float] do
+    String.to_float(json_value)
+  end
+
+  defp decode_value(json_value, :bytes = _type), do: Base.decode64!(json_value)
+
+  defp decode_value(json_value, {:enum, enum_mod} = _type) when is_integer(json_value) do
+    enum_mod.decode(json_value)
+  end
+
+  defp decode_value(json_value, {:enum, enum_mod} = _type) when is_binary(json_value) do
+    try do
+      as_atom = String.to_existing_atom(json_value)
+
+      if is_atom(enum_mod.encode(as_atom)) do
+        # It's quite a hack: as the generated encode/1 of an enum does not indicate
+        # if the given atom has been correctly encoded as a number, we check the type
+        # of the returned value to detect if `as_atom` is part of the enum.
+        raise Protox.JsonDecodingError.new(
+                enum_mod,
+                "#{json_value} is not a field of #{enum_mod}"
+              )
+      else
+        as_atom
+      end
+    rescue
+      ArgumentError ->
+        reraise Protox.JsonDecodingError.new(
+                  enum_mod,
+                  "#{json_value} is not a field of #{enum_mod}"
+                ),
+                __STACKTRACE__
+    end
+  end
+
+  defp decode_value(json_value, {:message, msg_mod}) do
+    Protox.JsonMessageDecoder.decode_message(msg_mod, json_value)
   end
 
   defp decode_value(json_value, _type), do: json_value
