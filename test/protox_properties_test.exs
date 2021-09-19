56d08b59e16be2cf98b55663a8e5650cba4af817
Alexandre Hamez <alexandre.hamez@gmail.com>
2021-09-19 12:33:52 +0200

    Property check of JSON encoding/decoding

--- test/protox_properties_test.exs
+++ test/protox_properties_test.exs
@@ -2,16 +2,29 @@ defmodule Protox.PropertiesTest do
   use ExUnit.Case
   use PropCheck
 
+  Code.require_file("test/support/messages.exs")
+  Code.require_file("test/support/random_init.exs")
+
   @moduletag :slow
 
   @tag :properties
-  property "Upper" do
-    forall {msg, encoded, encoded_bin, decoded} <- generate(Upper) do
+  property "Binary: Upper" do
+    forall {msg, encoded, encoded_bin, decoded} <- generate_binary(Upper) do
       is_list(encoded) and is_binary(encoded_bin) and decoded == msg
     end
   end
 
-  defp generate(mod) do
+  @tag :properties
+  property "JSON: Sub" do
+    # Use Sub as it doesn't contain any nestedd proto2 message
+    forall {msg, encoded, decoded} <- generate_json(Sub) do
+      is_list(encoded) and decoded == msg
+    end
+  end
+
+  # -- Private
+
+  defp generate_binary(mod) do
     let fields <- Protox.RandomInit.generate_fields(mod) do
       msg = Protox.RandomInit.generate_struct(mod, fields)
       encoded = Protox.encode!(msg)
@@ -19,6 +32,16 @@ defmodule Protox.PropertiesTest do
       decoded = Protox.decode!(encoded_bin, mod)
 
       {msg, encoded, encoded_bin, decoded}
+    end
+  end
+
+  defp generate_json(mod) do
+    let fields <- Protox.RandomInit.generate_fields(mod) do
+      msg = Protox.RandomInit.generate_struct(mod, fields)
+      encoded = Protox.json_encode!(msg)
+      decoded = Protox.json_decode!(encoded, mod)
+
+      {msg, encoded, decoded}
     end
   end
 end
