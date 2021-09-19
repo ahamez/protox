a337f278a859866fb60e1dbe195bb88c9c233af2
Alexandre Hamez <alexandre.hamez@gmail.com>
2021-09-19 09:22:55 +0200

    Loop around the test rather than inside

--- test/protox/json_decode_test.exs
+++ test/protox/json_decode_test.exs
@@ -1,98 +1,92 @@
-defmodule Protox.JsonDecodeTest do
+# A dedicated module to make sure all messages are compiled before Protox.JsonDecodeTest.
+defmodule Protox.JsonDecodeTestMessages do
   Code.require_file("./test/support/messages.exs")
+end
 
+defmodule Protox.JsonDecodeTest do
   use ExUnit.Case
   use Protox.Float
 
-  describe "Field names" do
-    test "Success: json field name is camel case" do
-      msg = %Msg{msg_e: true}
-      json = "{\"msgE\":true}"
-      assert Protox.json_decode!(json, Msg) == msg
-    end
-
-    test "Success: json field name is lower case" do
-      msg = %Msg{msg_e: true}
-      json = "{\"msg_e\":true}"
-      assert Protox.json_decode!(json, Msg) == msg
-    end
-  end
+  @scalar_tests %{
+    "{\"a\":null}" => {
+      %Sub{a: 0},
+      "null as default value"
+    },
+    "{\"a\":-1}" => {
+      %Sub{a: -1},
+      "int32 as number"
+    },
+    "{\"a\":-1.0}" => {
+      %FloatPrecision{a: -1.0},
+      "double as number"
+    },
+    "{\"a\":\"-1.0\"}" => {
+      %FloatPrecision{a: -1.0},
+      "double as string"
+    },
+    "{\"b\":-1.0}" => {
+      %FloatPrecision{b: -1.0},
+      "float as number"
+    },
+    "{\"b\":\"-1.0\"}" => {
+      %FloatPrecision{b: -1.0},
+      "float as string"
+    },
+    "{\"b\": \"foo\"}" => {
+      %Sub{b: "foo"},
+      "string"
+    },
+    "{\"m\":\"AQID\"}" => {
+      %Sub{m: <<1, 2, 3>>},
+      "bytes"
+    },
+    "{\"r\":\"FOO\"}" => {
+      %Sub{r: :FOO},
+      "enum as string"
+    },
+    "{\"r\":-1}" => {
+      %Sub{r: :NEG},
+      "enum as number"
+    },
+    "{\"r\":42}" => {
+      %Sub{r: 42},
+      "enum as unknown number"
+    },
+    "{\"c\":\"-1\", \"e\":\"-24\", \"l\":\"33\", \"s\":\"67\"}" =>
+      {%Sub{c: -1, e: -24, l: 33, s: 67}, "int64, sfixed64, uint64, fixed64 as string"},
+    "{\"c\":-1, \"e\":-24, \"l\":33, \"s\": 67}" =>
+      {%Sub{c: -1, e: -24, l: 33, s: 67}, "int64, sfixed64, uint64, fixed64 as numbers"},
+    "{\"msgF\": {\"a\": 1}}" => {
+      %Msg{msg_f: %Sub{a: 1}},
+      "nested messasge"
+    },
+    "{\"msgN\":\"foo\"}" => {
+      %Msg{msg_m: {:msg_n, "foo"}},
+      "oneof set to string"
+    },
+    "{\"msgO\":{\"a\":1}}" => {
+      %Msg{msg_m: {:msg_o, %Sub{a: 1}}},
+      "oneof set to message"
+    }
+  }
 
   describe "Scalar types" do
-    test "Success" do
-      scalar_tests = %{
-        "{\"a\":null}" => {
-          %Sub{a: 0},
-          "null as default value"
-        },
-        "{\"a\":-1}" => {
-          %Sub{a: -1},
-          "int32 as number"
-        },
-        "{\"a\":-1.0}" => {
-          %FloatPrecision{a: -1.0},
-          "double as number"
-        },
-        "{\"a\":\"-1.0\"}" => {
-          %FloatPrecision{a: -1.0},
-          "double as string"
-        },
-        "{\"b\":-1.0}" => {
-          %FloatPrecision{b: -1.0},
-          "float as number"
-        },
-        "{\"b\":\"-1.0\"}" => {
-          %FloatPrecision{b: -1.0},
-          "float as string"
-        },
-        "{\"b\": \"foo\"}" => {
-          %Sub{b: "foo"},
-          "string"
-        },
-        "{\"m\":\"AQID\"}" => {
-          %Sub{m: <<1, 2, 3>>},
-          "bytes"
-        },
-        "{\"r\":\"FOO\"}" => {
-          %Sub{r: :FOO},
-          "enum as string"
-        },
-        "{\"r\":-1}" => {
-          %Sub{r: :NEG},
-          "enum as number"
-        },
-        "{\"r\":42}" => {
-          %Sub{r: 42},
-          "enum as unknown number"
-        },
-        "{\"c\":\"-1\", \"e\":\"-24\", \"l\":\"33\", \"s\":\"67\"}" =>
-          {%Sub{c: -1, e: -24, l: 33, s: 67}, "int64, sfixed64, uint64, fixed64 as string"},
-        "{\"c\":-1, \"e\":-24, \"l\":33, \"s\": 67}" =>
-          {%Sub{c: -1, e: -24, l: 33, s: 67}, "int64, sfixed64, uint64, fixed64 as numbers"},
-        "{\"msgF\": {\"a\": 1}}" => {
-          %Msg{msg_f: %Sub{a: 1}},
-          "nested messasge"
-        },
-        "{\"msgN\":\"foo\"}" => {
-          %Msg{msg_m: {:msg_n, "foo"}},
-          "oneof set to string"
-        },
-        "{\"msgO\":{\"a\":1}}" => {
-          %Msg{msg_m: {:msg_o, %Sub{a: 1}}},
-          "oneof set to message"
-        }
-      }
+    for {json, {expected, description}} <- @scalar_tests do
+      test "Success: can decode #{description}" do
+        json = unquote(json)
+        expected = unquote(Macro.escape(expected))
 
-      for {json, {expected, description}} <- scalar_tests do
-        assert Protox.json_decode!(json, expected.__struct__) == expected,
-               "Cannot decode #{description}"
+        assert Protox.json_decode!(json, expected.__struct__) == expected
       end
     end
 
-    for {atom, string} <- [{:infinity, "Infinity"}, {:"-infinity", "-Infinity"}, {:nan, "NaN"}] do
+    for {value, string} <- [{:infinity, "Infinity"}, {:"-infinity", "-Infinity"}, {:nan, "NaN"}] do
       test "Success: double is #{string}" do
-        msg = %FloatPrecision{a: unquote(atom)}
-        json = "{\"a\":\"#{unquote(string)}\"}"
+        value = unquote(value)
+        string = unquote(string)
+
+        msg = %FloatPrecision{a: value}
+        json = "{\"a\":\"#{string}\"}"
         assert Protox.json_decode!(json, FloatPrecision) == msg
       end
     end
@@ -128,6 +122,20 @@ defmodule Protox.JsonDecodeTest do
       end
 
       assert {:error, %Protox.InvalidSyntax{}} = Protox.json_decode("", Protobuf2)
+    end
+  end
+
+  describe "Field names" do
+    test "Success: json field name is camel case" do
+      msg = %Msg{msg_e: true}
+      json = "{\"msgE\":true}"
+      assert Protox.json_decode!(json, Msg) == msg
+    end
+
+    test "Success: json field name is lower case" do
+      msg = %Msg{msg_e: true}
+      json = "{\"msg_e\":true}"
+      assert Protox.json_decode!(json, Msg) == msg
     end
   end
 
