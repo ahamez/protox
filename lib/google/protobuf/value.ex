2b9e29d9b8366780a157fef72007aed62afbc298
Alexandre Hamez <alexandre.hamez@gmail.com>
2021-09-29 21:46:15 +0200

    Extract NullValue into its own file

--- lib/google/protobuf/value.ex
+++ lib/google/protobuf/value.ex
@@ -3,18 +3,6 @@
 # JSON encoding
 # https://developers.google.com/protocol-buffers/docs/reference/google.protobuf#value
 
-defimpl Protox.JsonEnumDecoder, for: Google.Protobuf.NullValue do
-  def decode_enum(_enum, nil), do: :NULL_VALUE
-  def decode_enum(_enum, "NULL_VALUE"), do: :NULL_VALUE
-  def decode_enum(_enum, _value), do: raise(Protox.JsonDecodingError.new("invalid NullValue"))
-end
-
-defimpl Protox.JsonEnumEncoder, for: Google.Protobuf.NullValue do
-  def encode_enum(_enum, :NULL_VALUE, json_encode) do
-    json_encode.(nil)
-  end
-end
-
 defimpl Protox.JsonMessageDecoder, for: Google.Protobuf.Value do
   def decode_message(initial_message, value) when is_boolean(value) do
     struct!(initial_message, kind: {:bool_value, value})
