53c6b84231945cd3b9c8f0677f00b5da22b230c9
Alexandre Hamez <alexandre.hamez@gmail.com>
2024-11-26 20:38:20 +0100

    feat!: configure JSON library at compile time

--- test/support/messages.ex
+++ test/support/messages.ex
@@ -5,4 +5,20 @@ defmodule Protox.Messages do
     files: [
       "./test/samples/messages.proto"
     ]
+
+  use Protox,
+    files: [
+      "./test/samples/messages.proto",
+      "./test/samples/test_messages_proto3.proto"
+    ],
+    namespace: WithJason,
+    json_library: Jason
+
+  use Protox,
+    files: [
+      "./test/samples/messages.proto",
+      "./test/samples/test_messages_proto3.proto"
+    ],
+    namespace: WithPoison,
+    json_library: Poison
 end
