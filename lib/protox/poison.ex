b89303d078b66de3a1390531a2556c37f36463cf
Alexandre Hamez <alexandre.hamez@gmail.com>
2024-11-26 20:38:20 +0100

    feat!: configure JSON library at compile time

--- lib/protox/poison.ex
+++ lib/protox/poison.ex
@@ -2,45 +2,23 @@ defmodule Protox.Poison do
   @moduledoc false
   @behaviour Protox.JsonLibrary
 
-  if Code.ensure_loaded?(Poison) do
-    @impl true
-    def load(), do: :ok
-
-    @impl true
-    def decode!(iodata) do
-      try do
-        Poison.decode!(iodata)
-      rescue
-        e in [Poison.DecodeError, Poison.ParseError] ->
-          reraise Protox.JsonDecodingError.new(Exception.message(e)), __STACKTRACE__
-      end
+  @impl true
+  def decode!(iodata) do
+    try do
+      Poison.decode!(iodata)
+    rescue
+      e in [Poison.DecodeError, Poison.ParseError] ->
+        reraise Protox.JsonDecodingError.new(Exception.message(e)), __STACKTRACE__
     end
+  end
 
-    @impl true
-    def encode!(term) do
-      try do
-        Poison.encode!(term)
-      rescue
-        e in Poison.EncodeError ->
-          reraise Protox.JsonEncodingError.new(Exception.message(e)), __STACKTRACE__
-      end
-    end
-  else
-    @impl true
-    def load(), do: :error
-
-    @impl true
-    def decode!(_iodata) do
-      raise Protox.JsonDecodingError.new(
-              "Poison library not loaded. Please check your project dependencies."
-            )
-    end
-
-    @impl true
-    def encode!(_term) do
-      raise Protox.JsonEncodingError.new(
-              "Poison library not loaded. Please check your project dependencies."
-            )
+  @impl true
+  def encode!(term) do
+    try do
+      Poison.encode!(term)
+    rescue
+      e in Poison.EncodeError ->
+        reraise Protox.JsonEncodingError.new(Exception.message(e)), __STACKTRACE__
     end
   end
 end
