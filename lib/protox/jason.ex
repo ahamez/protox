429118322278d3488d1811d4389f174f5b0957f6
Alexandre Hamez <alexandre.hamez@gmail.com>
2024-11-27 18:55:50 +0100

    fix: compilation error when no JSON libraries are listed in deps

--- lib/protox/jason.ex
+++ lib/protox/jason.ex
@@ -1,24 +1,27 @@
 defmodule Protox.Jason do
   @moduledoc false
-  @behaviour Protox.JsonLibrary
 
-  @impl true
-  def decode!(iodata) do
-    try do
-      Jason.decode!(iodata)
-    rescue
-      e in [Jason.DecodeError, Protocol.UndefinedError] ->
-        reraise Protox.JsonDecodingError.new(Exception.message(e)), __STACKTRACE__
+  if Code.ensure_loaded?(Jason) do
+    @behaviour Protox.JsonLibrary
+
+    @impl true
+    def decode!(iodata) do
+      try do
+        Jason.decode!(iodata)
+      rescue
+        e in [Jason.DecodeError, Protocol.UndefinedError] ->
+          reraise Protox.JsonDecodingError.new(Exception.message(e)), __STACKTRACE__
+      end
     end
-  end
 
-  @impl true
-  def encode!(term) do
-    try do
-      Jason.encode_to_iodata!(term)
-    rescue
-      e in Jason.EncodeError ->
-        reraise Protox.JsonEncodingError.new(Exception.message(e)), __STACKTRACE__
+    @impl true
+    def encode!(term) do
+      try do
+        Jason.encode_to_iodata!(term)
+      rescue
+        e in Jason.EncodeError ->
+          reraise Protox.JsonEncodingError.new(Exception.message(e)), __STACKTRACE__
+      end
     end
   end
 end
--- lib/protox/json_library.ex
+++ lib/protox/json_library.ex
@@ -3,6 +3,8 @@ defmodule Protox.JsonLibrary do
   The behaviour to implement when wrapping a JSON library.
   """
 
+  @default_json_library Jason
+
   @doc """
   Should wrap any exception of the underlying library in Protox.JsonDecodingError.
   """
@@ -14,11 +16,13 @@ defmodule Protox.JsonLibrary do
   @callback encode!(term()) :: iodata() | no_return()
 
   @doc false
-  def get_wrapper(json_library) do
+  def get_wrapper(opts) do
+    json_library = Keyword.get(opts, :json_library, @default_json_library)
+
     if Code.ensure_loaded?(json_library) do
       Module.concat(Protox, json_library)
     else
-      raise Protox.JsonLibraryError.new()
+      nil
     end
   end
 end
