31b3eeefa818ed87ddd964c703d6692e01a6049e
Alexandre Hamez <alexandre.hamez@gmail.com>
2024-11-27 18:55:50 +0100

    fix: compilation error when no JSON libraries are listed in deps

--- lib/protox/poison.ex
+++ lib/protox/poison.ex
@@ -1,24 +1,27 @@
 defmodule Protox.Poison do
   @moduledoc false
-  @behaviour Protox.JsonLibrary
 
-  @impl true
-  def decode!(iodata) do
-    try do
-      Poison.decode!(iodata)
-    rescue
-      e in [Poison.DecodeError, Poison.ParseError] ->
-        reraise Protox.JsonDecodingError.new(Exception.message(e)), __STACKTRACE__
+  if Code.ensure_loaded?(Poison) do
+    @behaviour Protox.JsonLibrary
+
+    @impl true
+    def decode!(iodata) do
+      try do
+        Poison.decode!(iodata)
+      rescue
+        e in [Poison.DecodeError, Poison.ParseError] ->
+          reraise Protox.JsonDecodingError.new(Exception.message(e)), __STACKTRACE__
+      end
     end
-  end
 
-  @impl true
-  def encode!(term) do
-    try do
-      Poison.encode!(term)
-    rescue
-      e in Poison.EncodeError ->
-        reraise Protox.JsonEncodingError.new(Exception.message(e)), __STACKTRACE__
+    @impl true
+    def encode!(term) do
+      try do
+        Poison.encode!(term)
+      rescue
+        e in Poison.EncodeError ->
+          reraise Protox.JsonEncodingError.new(Exception.message(e)), __STACKTRACE__
+      end
     end
   end
 end
