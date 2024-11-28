02f04d723e1fc29402b48637984722e5a44d91f0
Alexandre Hamez <alexandre.hamez@gmail.com>
2024-11-28 20:14:26 +0100

    chore: parallel generation of benchmarks

--- benchmarks/generate_payloads.exs
+++ benchmarks/generate_payloads.exs
@@ -8,7 +8,7 @@ Code.compile_file("./benchmarks/handmade_payloads.exs")
 defmodule Protox.GeneratePayloads do
   use PropCheck
 
-  def generate(mod, size, min_sz, max_sz, nb \\ 1000) do
+  def generate(mod, size, min_sz, max_sz, nb \\ 100) do
     IO.puts("Generating for size=#{size}, min_sz=#{min_sz}, max_sz=#{max_sz}")
 
     gen =
@@ -37,19 +37,27 @@ defmodule Protox.GeneratePayloads do
   end
 end
 
-payloads = %{
-  upper_xsmall: Protox.GeneratePayloads.generate(Protox.Benchmarks.Upper, 1, 4, 128),
-  upper_small: Protox.GeneratePayloads.generate(Protox.Benchmarks.Upper, 2, 128, 512),
-  upper_medium: Protox.GeneratePayloads.generate(Protox.Benchmarks.Upper, 3, 512, 2048),
-  upper_large: Protox.GeneratePayloads.generate(Protox.Benchmarks.Upper, 5, 2048, 8192),
-  upper_xlarge: Protox.GeneratePayloads.generate(Protox.Benchmarks.Upper, 10, 8192, 262_144),
-  sub_xsmall: Protox.GeneratePayloads.generate(Protox.Benchmarks.Sub, 1, 4, 128),
-  sub_small: Protox.GeneratePayloads.generate(Protox.Benchmarks.Sub, 2, 128, 256),
-  sub_medium: Protox.GeneratePayloads.generate(Protox.Benchmarks.Sub, 10, 256, 512),
-  sub_large: Protox.GeneratePayloads.generate(Protox.Benchmarks.Sub, 20, 512, 1024),
-  sub_xlarge: Protox.GeneratePayloads.generate(Protox.Benchmarks.Sub, 50, 1024, 2048),
-  handmade: Protox.GeneratePayloads.handmade()
+payloads_async = %{
+  upper_xsmall: fn -> Protox.GeneratePayloads.generate(Protox.Benchmarks.Upper, 1, 4, 128) end,
+  upper_small: fn -> Protox.GeneratePayloads.generate(Protox.Benchmarks.Upper, 2, 128, 512) end,
+  upper_medium: fn -> Protox.GeneratePayloads.generate(Protox.Benchmarks.Upper, 3, 512, 2048) end,
+  upper_large: fn -> Protox.GeneratePayloads.generate(Protox.Benchmarks.Upper, 5, 2048, 8192) end,
+  upper_xlarge: fn ->
+    Protox.GeneratePayloads.generate(Protox.Benchmarks.Upper, 10, 8192, 262_144)
+  end,
+  sub_xsmall: fn -> Protox.GeneratePayloads.generate(Protox.Benchmarks.Sub, 1, 4, 128) end,
+  sub_small: fn -> Protox.GeneratePayloads.generate(Protox.Benchmarks.Sub, 2, 128, 256) end,
+  sub_medium: fn -> Protox.GeneratePayloads.generate(Protox.Benchmarks.Sub, 10, 256, 512) end,
+  sub_large: fn -> Protox.GeneratePayloads.generate(Protox.Benchmarks.Sub, 20, 512, 1024) end,
+  sub_xlarge: fn -> Protox.GeneratePayloads.generate(Protox.Benchmarks.Sub, 50, 1024, 2048) end,
+  handmade: fn -> Protox.GeneratePayloads.handmade() end
 }
+
+payloads =
+  payloads_async
+  |> Task.async_stream(fn {name, gen} -> {name, gen.()} end, timeout: :infinity)
+  |> Stream.map(fn {:ok, {name, payloads}} -> {name, payloads} end)
+  |> Map.new()
 
 "./benchmarks/payloads.bin"
 |> File.open!([:write])
