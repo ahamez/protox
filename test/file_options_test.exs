d6a65de3b11ad96aeb667f900651b6a60fb5aef9
Alexandre Hamez <alexandre.hamez@gmail.com>
2025-01-08 10:03:27 +0100

    test: fix flaky custom file options test

--- test/file_options_test.exs
+++ test/file_options_test.exs
@@ -1,6 +1,16 @@
 defmodule FileOptionsTest do
   use ExUnit.Case
 
+  # For a reason I don't understand, the custom option defined in
+  # custom_file_options.proto is not available if we add this file
+  # to the list of files to parse in `test/support/messages.ex`.
+  #
+  # However, if defined directly in the test file, it works fine.
+  use Protox,
+    files: [
+      "./test/samples/custom_file_options.proto"
+    ]
+
   test "Can read custom option from FileOptions" do
     file_options = MessageWithCustomFileOptions.file_options()
     assert Map.has_key?(file_options, :custom_field)
