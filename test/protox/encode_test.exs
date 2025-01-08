2f88193a7c20b0aa1d6fd40b3984bb5cecbac19d
Alexandre Hamez <alexandre.hamez@gmail.com>
2025-01-08 21:28:03 +0100

    test: use test_messages_proto{2,3}.proto
    Also
    1) generate test data for parsing tests on the fly
    2) remove more redundant tests

--- test/protox/encode_test.exs
+++ test/protox/encode_test.exs
@@ -1,19 +1,19 @@
 defmodule Protox.EncodeTest do
   use ExUnit.Case
 
-  alias ProtobufTestMessages.Proto3.TestAllTypesProto3
+  alias ProtobufTestMessages.Proto3.{NullHypothesisProto3, TestAllTypesProto3}
 
-  test "Empty" do
-    assert %Sub{} |> Protox.encode!() |> :binary.list_to_bin() == <<>>
+  test "Default TestAllTypesProto3" do
+    assert %TestAllTypesProto3{} |> Protox.encode!() |> :binary.list_to_bin() == <<>>
   end
 
-  test "Empty, with non throwing encode/1" do
-    {status, bytes} = Protox.encode(%Sub{})
+  test "Default TestAllTypesProto3, with non throwing encode/1" do
+    {status, bytes} = Protox.encode(%TestAllTypesProto3{})
     assert {:ok, []} == {status, List.flatten(bytes)}
   end
 
-  test "Empty, unknown fields are encoded back" do
-    msg = %Empty{
+  test "Messsage with no fields, unknown fields are encoded back" do
+    msg = %NullHypothesisProto3{
       __uf__: [
         {1, 0, "*"},
         {3, 1, <<246, 40, 92, 143, 194, 53, 69, 64>>},
@@ -25,191 +25,108 @@ defmodule Protox.EncodeTest do
              <<8, 42, 25, 246, 40, 92, 143, 194, 53, 69, 64, 136, 241, 4, 83>>
   end
 
-  test "Sub.a" do
-    assert %Sub{a: 150} |> Protox.encode!() |> :binary.list_to_bin() == <<8, 150, 1>>
+  test "Scalar int64" do
+    assert %TestAllTypesProto3{optional_int64: -300} |> Protox.encode!() |> :binary.list_to_bin() ==
+             <<16, 212, 253, 255, 255, 255, 255, 255, 255, 255, 1>>
   end
 
-  test "Sub.a, negative" do
-    assert %Sub{a: -150} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<8, 234, 254, 255, 255, 255, 255, 255, 255, 255, 1>>
+  test "Scalar uint32" do
+    assert %TestAllTypesProto3{optional_uint32: 42} |> Protox.encode!() |> :binary.list_to_bin() ==
+             <<24, 42>>
   end
 
-  test "Sub.b" do
-    assert %Sub{b: "testing"} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<18, 7, 116, 101, 115, 116, 105, 110, 103>>
-  end
-
-  test "Sub.c" do
-    assert %Sub{c: -300} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<48, 212, 253, 255, 255, 255, 255, 255, 255, 255, 1>>
-  end
-
-  test "Sub.d; Sub.e" do
-    assert %Sub{d: 901, e: 433} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<56, 133, 7, 64, 177, 3>>
-  end
-
-  test "Sub.f" do
-    assert %Sub{f: -1323} |> Protox.encode!() |> :binary.list_to_bin() == <<72, 213, 20>>
-  end
-
-  test "Sub.g" do
-    assert %Sub{g: [1, 2]} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<106, 16, 1, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0>>
-  end
-
-  test "Sub.g, negative" do
-    assert %Sub{g: [1, -2]} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<106, 16, 1, 0, 0, 0, 0, 0, 0, 0, 254, 255, 255, 255, 255, 255, 255, 255>>
-  end
-
-  test "Sub.g; Sub.h; Sub.i" do
-    assert %Sub{g: [0], h: [-1], i: [33.2, -44.0]}
+  test "Scalar uint64" do
+    assert %TestAllTypesProto3{optional_uint64: 300_000}
            |> Protox.encode!()
            |> :binary.list_to_bin() ==
-             <<106, 8, 0, 0, 0, 0, 0, 0, 0, 0, 114, 4, 255, 255, 255, 255, 122, 16, 154, 153, 153,
-               153, 153, 153, 64, 64, 0, 0, 0, 0, 0, 0, 70, 192>>
+             <<32, 224, 167, 18>>
   end
 
-  test "Sub.i, infinity, -infinity, nan" do
-    assert %Sub{i: [:infinity, :"-infinity", :nan]}
+  test "Scalar sint64" do
+    assert %TestAllTypesProto3{optional_sint64: -1323}
+           |> Protox.encode!()
+           |> :binary.list_to_bin() == <<48, 213, 20>>
+  end
+
+  test "Scalar fixed32" do
+    assert %TestAllTypesProto3{optional_fixed32: 352}
+           |> Protox.encode!()
+           |> :binary.list_to_bin() == <<61, 96, 1, 0, 0>>
+  end
+
+  test "Scalar sfixed64" do
+    assert %TestAllTypesProto3{optional_sfixed64: -352}
+           |> Protox.encode!()
+           |> :binary.list_to_bin() == <<81, 160, 254, 255, 255, 255, 255, 255, 255>>
+  end
+
+  test "Repeated int32" do
+    assert %TestAllTypesProto3{repeated_int32: [-1, 0, 1]}
            |> Protox.encode!()
            |> :binary.list_to_bin() ==
-             <<122, 24, 0, 0, 0, 0, 0, 0, 0xF0, 0x7F, 0, 0, 0, 0, 0, 0, 0xF0, 0xFF, 0, 0, 0, 0, 0,
-               1, 241, 255>>
+             <<250, 1, 12, 255, 255, 255, 255, 255, 255, 255, 255, 255, 1, 0, 1>>
   end
 
-  test "Sub.h" do
-    assert %Sub{h: [-1, -2]} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<114, 8, 255, 255, 255, 255, 254, 255, 255, 255>>
-  end
-
-  test "Sub.j, unpacked in definition" do
-    assert %Sub{j: [1, 2, 3]} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<128, 1, 1, 128, 1, 2, 128, 1, 3>>
-  end
-
-  test "Sub.k; Sub.l" do
-    assert %Sub{k: 23, l: -99} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<141, 1, 23, 0, 0, 0, 145, 1, 157, 255, 255, 255, 255, 255, 255, 255>>
-  end
-
-  test "Sub.m, empty" do
-    assert %Sub{m: <<>>} |> Protox.encode!() |> :binary.list_to_bin() == <<>>
-  end
-
-  test "Sub.m" do
-    assert %Sub{m: <<1, 2, 3>>} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<154, 1, 3, 1, 2, 3>>
-  end
-
-  test "Sub.n" do
-    assert %Sub{n: [true, false, false, true]} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<162, 1, 4, 1, 0, 0, 1>>
-  end
-
-  test "Sub.n (all true)" do
-    assert %Sub{n: [true, true, true, true]} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<162, 1, 4, 1, 1, 1, 1>>
-  end
-
-  test "Sub.n (all false)" do
-    assert %Sub{n: [false, false, false, false]}
-           |> Protox.encode!()
-           |> :binary.list_to_bin() == <<162, 1, 4, 0, 0, 0, 0>>
-  end
-
-  test "Sub.o " do
-    assert %Sub{o: [:FOO, :BAR, :FOO]} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<170, 1, 3, 0, 1, 0>>
-  end
-
-  test "Sub.o (unknown entry) " do
-    assert %Sub{o: [:FOO, :BAR, :FOO, 2]} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<170, 1, 4, 0, 1, 0, 2>>
-  end
-
-  test "Sub.p (unpacked in definition) " do
-    assert %Sub{p: [true, false, true, false]} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<176, 1, 1, 176, 1, 0, 176, 1, 1, 176, 1, 0>>
-  end
-
-  test "Sub.q (unpacked in definition) " do
-    assert %Sub{q: [:FOO, :BAR, :BAR, :FOO]} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<184, 1, 0, 184, 1, 1, 184, 1, 1, 184, 1, 0>>
-  end
-
-  test "Sub.q (unpacked in definition, with unknown values) " do
-    assert %Sub{q: [:FOO, :BAR, 2, :FOO]} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<184, 1, 0, 184, 1, 1, 184, 1, 2, 184, 1, 0>>
-  end
-
-  test "Sub.r, negative constant" do
-    assert %Sub{r: :NEG} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<192, 1, 255, 255, 255, 255, 255, 255, 255, 255, 255, 1>>
-  end
-
-  test "Sub.z" do
-    assert %Sub{z: -20} |> Protox.encode!() |> :binary.list_to_bin() == <<136, 241, 4, 39>>
-  end
-
-  test "Sub, unknown fields (double)" do
-    assert %Sub{
-             a: 42,
-             b: "",
-             z: -42,
-             __uf__: [{3, 1, <<246, 40, 92, 143, 194, 53, 69, 64>>}]
-           }
+  test "Repeated fixed64" do
+    assert %TestAllTypesProto3{repeated_fixed64: [0, 1]}
            |> Protox.encode!()
            |> :binary.list_to_bin() ==
-             <<8, 42, 136, 241, 4, 83, 25, 246, 40, 92, 143, 194, 53, 69, 64>>
+             <<178, 2, 16, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0>>
   end
 
-  test "Sub, unknown tag (embedded message)" do
-    assert %Sub{a: 42, b: "", z: -42, __uf__: [{4, 2, <<>>}]}
-           |> Protox.encode!()
-           |> :binary.list_to_bin() == <<8, 42, 136, 241, 4, 83, 34, 0>>
-  end
-
-  test "Sub, unknown tag (string)" do
-    assert %Sub{
-             a: 42,
-             b: "",
-             z: -42,
-             __uf__: [{5, 2, <<121, 97, 121, 101>>}]
-           }
-           |> Protox.encode!()
-           |> :binary.list_to_bin() == <<8, 42, 136, 241, 4, 83, 42, 4, 121, 97, 121, 101>>
-  end
-
-  test "Sub, unknown tag (bytes)" do
-    bytes = fn -> <<0>> end |> Stream.repeatedly() |> Enum.take(128) |> Enum.join()
-
-    assert %Sub{a: 3342, b: "", z: -10, __uf__: [{10, 2, bytes}]}
-           |> Protox.encode!()
-           |> :binary.list_to_bin() == <<8, 142, 26, 136, 241, 4, 19, 82, 128, 1, bytes::binary>>
-  end
-
-  test "Sub, unknown tag (varint + bytes)" do
-    assert %Sub{
-             a: 3342,
-             b: "",
-             z: -10,
-             __uf__: [
-               {11, 0, <<154, 5>>},
-               {10, 2, <<104, 101, 121, 33>>}
-             ]
-           }
+  test "Repeated sfixed32" do
+    assert %TestAllTypesProto3{repeated_sfixed32: [-1, 2]}
            |> Protox.encode!()
            |> :binary.list_to_bin() ==
-             <<8, 142, 26, 136, 241, 4, 19, 88, 154, 5, 82, 4, 104, 101, 121, 33>>
+             <<186, 2, 8, 255, 255, 255, 255, 2, 0, 0, 0>>
   end
 
-  test "Sub, unknown tag (float + varint + bytes)" do
-    assert %Sub{
-             a: 3342,
-             b: "",
-             z: -10,
+  test "Repeated double" do
+    assert %TestAllTypesProto3{repeated_double: [33.2, -44.0, :infinity, :"-infinity", :nan]}
+           |> Protox.encode!()
+           |> :binary.list_to_bin() ==
+             <<210, 2, 40, 154, 153, 153, 153, 153, 153, 64, 64, 0, 0, 0, 0, 0, 0, 70, 192, 0, 0,
+               0, 0, 0, 0, 240, 127, 0, 0, 0, 0, 0, 0, 240, 255, 0, 0, 0, 0, 0, 1, 241, 255>>
+  end
+
+  test "Repeated float" do
+    assert %TestAllTypesProto3{repeated_float: [33.2, -44.0, :infinity, :"-infinity", :nan]}
+           |> Protox.encode!()
+           |> :binary.list_to_bin() ==
+             <<202, 2, 20, 205, 204, 4, 66, 0, 0, 48, 194, 0, 0, 128, 127, 0, 0, 128, 255, 0, 1,
+               129, 255>>
+  end
+
+  test "Repeated bool" do
+    assert %TestAllTypesProto3{repeated_bool: [true, false, true, false, false, false]}
+           |> Protox.encode!()
+           |> :binary.list_to_bin() ==
+             <<218, 2, 6, 1, 0, 1, 0, 0, 0>>
+  end
+
+  test "Repeated enum" do
+    assert %TestAllTypesProto3{repeated_nested_enum: [:FOO, :BAR, :BAZ, 4]}
+           |> Protox.encode!()
+           |> :binary.list_to_bin() ==
+             <<154, 3, 4, 0, 1, 2, 4>>
+  end
+
+  test "Unpacked Repeated int32" do
+    assert %TestAllTypesProto3{unpacked_int32: [-1, 2, 3]}
+           |> Protox.encode!()
+           |> :binary.list_to_bin() ==
+             <<200, 5, 255, 255, 255, 255, 255, 255, 255, 255, 255, 1, 200, 5, 2, 200, 5, 3>>
+  end
+
+  test "Bytes" do
+    assert %TestAllTypesProto3{optional_bytes: <<1, 2, 3>>}
+           |> Protox.encode!()
+           |> :binary.list_to_bin() ==
+             <<122, 3, 1, 2, 3>>
+  end
+
+  test "Unknown fields (float + varint + bytes)" do
+    assert %TestAllTypesProto3{
              __uf__: [
                {12, 5, <<236, 81, 5, 66>>},
                {11, 0, <<154, 5>>},
@@ -218,164 +135,18 @@ defmodule Protox.EncodeTest do
            }
            |> Protox.encode!()
            |> :binary.list_to_bin() ==
-             <<8, 142, 26, 136, 241, 4, 19, 101, 236, 81, 5, 66, 88, 154, 5, 82, 4, 104, 101, 121,
-               33>>
+             <<101, 236, 81, 5, 66, 88, 154, 5, 82, 4, 104, 101, 121, 33>>
   end
 
-  test "Msg.msg_d, :FOO" do
-    assert %Msg{msg_d: :FOO} |> Protox.encode!() |> :binary.list_to_bin() == <<>>
-  end
-
-  test "Msg.msg_d, :BAR" do
-    assert %Msg{msg_d: :BAR} |> Protox.encode!() |> :binary.list_to_bin() == <<8, 1>>
-  end
-
-  test "Msg.msg_d, :BAZ" do
-    assert %Msg{msg_d: :BAZ} |> Protox.encode!() |> :binary.list_to_bin() == <<8, 1>>
-  end
-
-  test "Msg.msg_d, unknown value" do
-    assert %Msg{msg_d: 99} |> Protox.encode!() |> :binary.list_to_bin() == <<8, 99>>
-  end
-
-  test "Msg.msg_e, false" do
-    assert %Msg{msg_e: false} |> Protox.encode!() |> :binary.list_to_bin() == <<>>
-  end
-
-  test "Msg.msg_e, true" do
-    assert %Msg{msg_e: true} |> Protox.encode!() |> :binary.list_to_bin() == <<16, 1>>
-  end
-
-  test "Msg.msg_f, empty" do
-    assert %Msg{msg_f: %Sub{}} |> Protox.encode!() |> :binary.list_to_bin() == <<26, 0>>
-  end
-
-  test "Msg.msg_f.a" do
-    assert %Msg{msg_f: %Sub{a: 150}} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<26, 3, 8, 150, 1>>
-  end
-
-  test "Msg.msg_g, empty" do
-    assert %Msg{msg_g: []} |> Protox.encode!() |> :binary.list_to_bin() == <<>>
-  end
-
-  test "Msg.msg_g (negative)" do
-    assert %Msg{msg_g: [1, 2, -3]} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<34, 12, 1, 2, 253, 255, 255, 255, 255, 255, 255, 255, 255, 1>>
-  end
-
-  test "Msg.msg_g" do
-    assert %Msg{msg_g: [1, 2, 3]} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<34, 3, 1, 2, 3>>
-  end
-
-  test "Msg.msg_h, empty" do
-    assert %Msg{msg_h: 0.0} |> Protox.encode!() |> :binary.list_to_bin() == <<>>
-  end
-
-  test "Msg.msg_h" do
-    assert %Msg{msg_h: -43.2} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<41, 154, 153, 153, 153, 153, 153, 69, 192>>
-  end
-
-  test "Msg.msg_i" do
-    assert %Msg{msg_i: [2.3, -4.2]} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<50, 8, 51, 51, 19, 64, 102, 102, 134, 192>>
-  end
-
-  test "Msg.msg_i, infinity, -infinity, nan" do
-    assert %Msg{msg_i: [:infinity, :"-infinity", :nan]}
-           |> Protox.encode!()
-           |> :binary.list_to_bin() ==
-             <<50, 12, 0, 0, 0x80, 0x7F, 0, 0, 0x80, 0xFF, 0, 1, 129, 255>>
-  end
-
-  test "Msg.msg_j" do
-    assert %Msg{msg_j: [%Sub{a: 42}, %Sub{b: "foo"}]}
-           |> Protox.encode!()
-           |> :binary.list_to_bin() == <<58, 2, 8, 42, 58, 5, 18, 3, 102, 111, 111>>
-  end
-
-  test "Msg.msg_k" do
-    assert %Msg{msg_k: %{1 => "foo", 2 => "bar"}}
-           |> Protox.encode!()
-           |> :binary.list_to_bin() ==
-             <<66, 7, 8, 1, 18, 3, 102, 111, 111, 66, 7, 8, 2, 18, 3, 98, 97, 114>>
-  end
-
-  test "Msg.msg_l" do
-    assert %Msg{msg_l: %{"bar" => 1.0, "foo" => 43.2}}
-           |> Protox.encode!()
-           |> :binary.list_to_bin() ==
-             <<74, 14, 10, 3, 98, 97, 114, 17, 0, 0, 0, 0, 0, 0, 240, 63, 74, 14, 10, 3, 102, 111,
-               111, 17, 154, 153, 153, 153, 153, 153, 69, 64>>
-  end
-
-  test "Msg.msg_m, string" do
-    assert %Msg{msg_m: {:msg_n, "bar"}} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<82, 3, 98, 97, 114>>
-  end
-
-  test "Msg.msg_m, Sub" do
-    assert %Msg{msg_m: {:msg_o, %Sub{a: 42}}} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<90, 2, 8, 42>>
-  end
-
-  test "Msg.msg_p" do
-    assert %Msg{msg_p: %{1 => :BAR}} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<98, 4, 8, 1, 16, 1>>
-  end
-
-  test "Msg.msg_q" do
-    assert %Msg{msg_q: nil} |> Protox.encode!() |> :binary.list_to_bin() == <<>>
-  end
-
-  test "Msg.msg_oneof_double" do
-    assert %Msg{msg_oneof_field: {:msg_oneof_double, 0}}
-           |> Protox.encode!()
-           |> :binary.list_to_bin() == <<177, 7, 0, 0, 0, 0, 0, 0, 0, 0>>
-  end
-
-  test "Sub with all fields unset serializes to <<>>" do
-    assert %Empty{} |> Protox.encode!() |> :binary.list_to_bin() == <<>>
-  end
-
-  test "Upper.empty" do
-    assert %Upper{empty: %Empty{}} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<26, 0>>
-  end
-
-  test "Protobuf2.a, unset" do
-    assert %Protobuf2{a: nil} |> Protox.encode!() |> :binary.list_to_bin() == <<>>
-  end
-
-  test "Protobuf2.s, default" do
-    assert %Protobuf2{s: :TWO} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<200, 1, 2>>
-  end
-
-  test "Protobuf2.s" do
-    assert %Protobuf2{s: :ONE} |> Protox.encode!() |> :binary.list_to_bin() ==
-             <<200, 1, 1>>
-  end
-
-  test "Protobuf2.t, default is nil" do
-    assert %Protobuf2{t: nil} |> Protox.encode!() |> :binary.list_to_bin() == <<>>
+  test "Empty repeated bool" do
+    assert %TestAllTypesProto3{repeated_bool: []} |> Protox.encode!() |> :binary.list_to_bin() ==
+             <<>>
   end
 
   test "Optional sub message" do
     assert %OptionalUpperMsg{sub: %OptionalSubMsg{a: 42}}
            |> Protox.encode!()
            |> :binary.list_to_bin() == <<10, 2, 8, 42>>
-  end
-
-  test "Optional sub message set to nil" do
-    assert %OptionalUpperMsg{sub: nil} |> Protox.encode!() |> :binary.list_to_bin() == <<>>
-  end
-
-  test "Required" do
-    assert %Required{a: 0} |> Protox.encode!() |> :binary.list_to_bin() == <<8, 0>>
-    assert %Required{a: 1} |> Protox.encode!() |> :binary.list_to_bin() == <<8, 1>>
   end
 
   test "Do not output default double/float" do
@@ -393,7 +164,7 @@ defmodule Protox.EncodeTest do
   test "Raise when required field is missing" do
     exception =
       assert_raise Protox.RequiredFieldsError, "Some required fields are not set: [:a]", fn ->
-        Protox.encode!(%Required{})
+        Protox.encode!(%Protobuf2Required{})
       end
 
     assert exception.missing_fields == [:a]
@@ -401,14 +172,8 @@ defmodule Protox.EncodeTest do
 
   test "UTF-8 strings" do
     [
-      {
-        "",
-        <<>>
-      },
-      {
-        "hello, 漢字, 💻, 🏁, working fine",
-        <<114, 39, "hello, 漢字, 💻, 🏁, working fine">>
-      }
+      {"", <<>>},
+      {"hello, 漢字, 💻, 🏁, working fine", <<114, 39, "hello, 漢字, 💻, 🏁, working fine">>}
     ]
     |> Enum.each(fn {string, expected_encoded_msg} ->
       assert %TestAllTypesProto3{optional_string: string}
--- test/protox/parse_test.exs
+++ test/protox/parse_test.exs
@@ -2,103 +2,75 @@ defmodule Protox.ParseTest do
   use ExUnit.Case
 
   setup_all do
-    data = File.read!("./test/samples/file_descriptor_set.bin")
+    file_descriptor_set_bin_path = Protox.TmpFs.tmp_file_path!(".bin")
+    File.rm_rf!(file_descriptor_set_bin_path)
+
+    {"", 0} =
+      System.cmd("protoc", [
+        "--include_imports",
+        "-o",
+        file_descriptor_set_bin_path,
+        "./test/samples/google/test_messages_proto3.proto",
+        "./test/samples/google/test_messages_proto2.proto"
+      ])
+
+    data = File.read!(file_descriptor_set_bin_path)
+    File.rm_rf!(file_descriptor_set_bin_path)
+
     %{enums: enums, messages: messages} = Protox.Parse.parse(data)
 
     {:ok, enums: enums, messages: messages}
   end
 
   test "Parse FileDescriptorSet, protobuf 3 enums", %{enums: enums} do
-    {_, constants} = Enum.find(enums, fn {name, _} -> name == Abc.Def.MyForeignEnum end)
-    assert constants == [{0, :FOREIGN_ZERO}, {1, :FOREIGN_ONE}, {1, :FOREIGN_ONE_BIS}]
+    {_, constants} =
+      Enum.find(enums, fn {name, _} -> name == ProtobufTestMessages.Proto3.ForeignEnum end)
 
-    {_, constants} = Enum.find(enums, fn {name, _} -> name == Abc.Def.Proto3.NestedEnum end)
-    assert constants == [{0, :FOO}, {2, :BAR}]
+    assert constants == [{0, :FOREIGN_FOO}, {1, :FOREIGN_BAR}, {2, :FOREIGN_BAZ}]
+
+    {_, constants} =
+      Enum.find(enums, fn {name, _} ->
+        name == ProtobufTestMessages.Proto3.TestAllTypesProto3.NestedEnum
+      end)
+
+    assert constants == [{0, :FOO}, {1, :BAR}, {2, :BAZ}, {-1, :NEG}]
   end
 
   test "Parse FileDescriptorSet, protobuf 3 messages", %{messages: messages} do
-    assert syntax(messages, Abc.Def.Proto3) == :proto3
-    fs = fields(messages, Abc.Def.Proto3)
-    assert field(fs, 1) == {:repeated, :repeated_int32, :packed, :int32}
-    assert field(fs, 2) == {:optional, :double, {:scalar, 0}, :double}
-    assert field(fs, 3) == {nil, :map_sfixed32_fixed64, :map, {:sfixed32, :fixed64}}
-    assert field(fs, 4) == {:optional, :oneof_1_int32, {:oneof, :oneof_1}, :int32}
-    assert field(fs, 5) == {:optional, :oneof_1_double, {:oneof, :oneof_1}, :double}
+    assert syntax(messages, ProtobufTestMessages.Proto3.TestAllTypesProto3) == :proto3
 
-    assert field(fs, 6) ==
-             {:optional, :oneof_1_foreign_enum, {:oneof, :oneof_1},
-              {:enum, Abc.Def.MyForeignEnum}}
+    fs = fields(messages, ProtobufTestMessages.Proto3.TestAllTypesProto3)
 
-    assert field(fs, 7) == {:optional, :oneof_1_proto2a, {:oneof, :oneof_1}, {:message, Proto2A}}
+    assert field(fs, 12) == {:optional, :optional_double, {:scalar, 0}, :double}
+    assert field(fs, 31) == {:repeated, :repeated_int32, :packed, :int32}
+    assert field(fs, 32) == {:repeated, :repeated_int64, :packed, :int64}
+    assert field(fs, 64) == {nil, :map_sfixed32_sfixed32, :map, {:sfixed32, :sfixed32}}
+    assert field(fs, 75) == {:repeated, :packed_int32, :packed, :int32}
+    assert field(fs, 89) == {:repeated, :unpacked_int32, :unpacked, :int32}
+    assert field(fs, 111) == {:optional, :oneof_uint32, {:oneof, :oneof_field}, :uint32}
 
-    assert field(fs, 8) == {nil, :map_string_proto2a, :map, {:string, {:message, Proto2A}}}
+    assert field(fs, 119) ==
+             {:optional, :oneof_enum, {:oneof, :oneof_field},
+              {:enum, ProtobufTestMessages.Proto3.TestAllTypesProto3.NestedEnum}}
 
-    assert field(fs, 9) == {:optional, :bytes, {:scalar, <<>>}, :bytes}
-
-    assert field(fs, 10) ==
-             {nil, :map_int64_nested_enum, :map, {:int64, {:enum, Abc.Def.Proto3.NestedEnum}}}
-
-    assert field(fs, 134) == {:optional, :oneof_2_int32, {:oneof, :oneof_2}, :int32}
-
-    assert field(fs, 135) ==
-             {:optional, :oneof_2_nested_enum, {:oneof, :oneof_2},
-              {:enum, Abc.Def.Proto3.NestedEnum}}
-
-    assert field(fs, 9999) ==
-             {:optional, :nested_enum, {:scalar, :FOO}, {:enum, Abc.Def.Proto3.NestedEnum}}
-
-    assert syntax(messages, Abc.Def.EmptyProto3) == :proto3
-    assert [] = fields(messages, Abc.Def.EmptyProto3)
+    assert [] = fields(messages, ProtobufTestMessages.Proto3.NullHypothesisProto3)
   end
 
   test "Parse FileDescriptorSet, protobuf 2 enums", %{enums: enums} do
-    {_, constants} = Enum.find(enums, fn {name, _} -> name == Proto2A.NestedEnum end)
-    assert constants == [{0, :N_ZERO}, {3, :N_THREE}]
+    {_, constants} =
+      Enum.find(enums, fn {name, _} -> name == ProtobufTestMessages.Proto2.ForeignEnumProto2 end)
+
+    assert constants == [{0, :FOREIGN_FOO}, {1, :FOREIGN_BAR}, {2, :FOREIGN_BAZ}]
   end
 
   test "Parse FileDescriptorSet, protobuf 2 messages", %{messages: messages} do
-    assert syntax(messages, Proto2A.NestedMessage) == :proto2
-    fs = fields(messages, Proto2A.NestedMessage)
-    assert field(fs, 1) == {:required, :required_string, {:scalar, "foo"}, :string}
-    assert field(fs, 2) == {:optional, :optional_float, {:scalar, -1.1}, :float}
-    assert field(fs, 3) == {:optional, :optional_fixed64, {:scalar, 32_108}, :fixed64}
+    assert syntax(messages, ProtobufTestMessages.Proto2.TestAllRequiredTypesProto2) == :proto2
 
-    assert syntax(messages, Proto2A) == :proto2
-    fs = fields(messages, Proto2A)
-    assert field(fs, 1) == {:repeated, :repeated_int32_packed, :packed, :int32}
-    assert field(fs, 2) == {:repeated, :repeated_int32_unpacked, :unpacked, :int32}
+    fs = fields(messages, ProtobufTestMessages.Proto2.TestAllRequiredTypesProto2)
 
-    assert field(fs, 3) ==
-             {:optional, :optional_nested_message, {:scalar, nil},
-              {:message, Proto2A.NestedMessage}}
-
-    assert field(fs, 4) ==
-             {:repeated, :repeated_nested_enum, :unpacked, {:enum, Proto2A.NestedEnum}}
-
-    assert field(fs, 5) ==
-             {:repeated, :repeated_nested_message, :unpacked, {:message, Proto2A.NestedMessage}}
-
-    assert field(fs, 6) == {:optional, :bytes, {:scalar, <<96, 118>>}, :bytes}
-    assert field(fs, 126) == {:optional, :extension_int32, {:scalar, 0}, :int32}
-    assert field(fs, 199) == {:optional, :extension_double, {:scalar, 42.42}, :double}
-
-    assert syntax(messages, Proto2B) == :proto2
-    fs = fields(messages, Proto2B)
-
-    assert field(fs, 1) ==
-             {:optional, :optional_proto2a_nested_enum, {:scalar, :N_ZERO},
-              {:enum, Proto2A.NestedEnum}}
-
-    assert field(fs, 2) ==
-             {:required, :required_proto2a_nested_enum, {:scalar, :N_THREE},
-              {:enum, Proto2A.NestedEnum}}
-  end
-
-  test "Parse Proto3, packed and unpacked fields", %{messages: messages} do
-    fs = fields(messages, Abc.Def.Proto3)
-
-    assert field(fs, 200) == {:repeated, :repeated_int32_packed, :packed, :int32}
-    assert field(fs, 201) == {:repeated, :repeated_int32_unpacked, :unpacked, :int32}
+    assert field(fs, 1) == {:required, :required_int32, {:scalar, 0}, :int32}
+    assert field(fs, 11) == {:required, :required_float, {:scalar, 0.0}, :float}
+    assert field(fs, 15) == {:required, :required_bytes, {:scalar, <<>>}, :bytes}
   end
 
   defp field(fields, tag) do
