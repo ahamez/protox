95f4d0330c353fd51e1405b8ab83029b5326d57a
Alexandre Hamez <alexandre.hamez@gmail.com>
2025-01-18 16:23:48 +0100

    refactor: compute FileOptions at compile time

--- lib/protox/message.ex
+++ lib/protox/message.ex
@@ -5,7 +5,9 @@ defmodule Protox.Message do
           name: atom(),
           syntax: atom(),
           fields: list(Protox.Field.t()),
-          file_options: any()
+          # :file_options is first created from a message Google.Protobuf.FileOptions,
+          # then it's transformed into a map, hence the two different types.
+          file_options: struct() | %{atom() => any()}
         }
 
   @enforced_keys [:name, :syntax, :fields]
