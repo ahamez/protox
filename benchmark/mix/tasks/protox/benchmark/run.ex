12f14ed6e4bb28a024fcc4e2103cb3dafcab033f
Alexandre Hamez <alexandre.hamez@gmail.com>
2025-01-23 16:43:31 +0100

    chore: can choose which task to run in benchmark

--- benchmark/mix/tasks/protox/benchmark/run.ex
+++ benchmark/mix/tasks/protox/benchmark/run.ex
@@ -4,17 +4,18 @@ defmodule Mix.Tasks.Protox.Benchmark.Run do
   use Mix.Task
 
   @options [
-    tag: :string
+    tag: :string,
+    task: :string
   ]
 
   @impl Mix.Task
   @spec run(any) :: any
   def run(args) do
     with {opts, _argv, []} <- OptionParser.parse(args, strict: @options),
+         tasks <- get_tasks(opts),
          tag <- get_tag(opts),
          payloads <- get_payloads("./benchmark/benchmark_payloads.bin") do
-      run_benchee(tag, payloads, :encode)
-      run_benchee(tag, payloads, :decode)
+      run_benchee_tasks(tag, payloads, tasks)
     else
       err ->
         IO.puts(:stderr, "Error: #{inspect(err)}")
@@ -22,44 +23,58 @@ defmodule Mix.Tasks.Protox.Benchmark.Run do
     end
   end
 
-  defp run_benchee(tag, payloads, task) do
-    jobs =
-      case task do
-        :encode ->
-          %{
-            "encode" => fn input ->
-              Enum.map(input, fn {msg, _size, _bytes} -> msg.__struct__.encode!(msg) end)
-            end
-          }
+  defp run_benchee_tasks(tag, payloads, tasks) do
+    Enum.each(tasks, fn task ->
+      job =
+        case task do
+          :encode ->
+            %{
+              encode: fn input ->
+                Enum.map(input, fn {msg, _size, _bytes} -> msg.__struct__.encode!(msg) end)
+              end
+            }
 
-        :decode ->
-          %{
-            "decode" => fn input ->
-              Enum.map(input, fn {msg, _size, bytes} -> msg.__struct__.decode!(bytes) end)
-            end
-          }
-      end
+          :decode ->
+            %{
+              decode: fn input ->
+                Enum.map(input, fn {msg, _size, bytes} -> msg.__struct__.decode!(bytes) end)
+              end
+            }
+        end
 
+      run_benchee(tag, payloads, task, job)
+    end)
+  end
+
+  defp run_benchee(tag, payloads, task, job) do
     Benchee.run(
-      jobs,
+      job,
       inputs: payloads,
       save: [
-        path: Path.join(["./benchmark", "#{task}-#{tag}.benchee"]),
+        path: Path.join(["./benchmark/output/benchee", "#{task}-#{tag}.benchee"]),
         tag: "#{task}-#{tag}"
       ],
-      load: ["./benchmark/#{task}*.benchee"],
+      load: ["./benchmark/output/benchee/#{task}*.benchee"],
       time: 5,
       memory_time: 2,
       reduction_time: 2,
       formatters: [
-        {Benchee.Formatters.HTML, file: "benchmark/output/#{task}-#{tag}.html"},
+        {Benchee.Formatters.HTML, file: "benchmark/output/html/#{task}-#{tag}.html"},
         Benchee.Formatters.Console
       ]
     )
   end
 
+  defp get_tasks(opts) do
+    case Keyword.get(opts, :task, nil) do
+      nil -> [:encode, :decode]
+      "encode" -> [:encode]
+      "decode" -> [:decode]
+    end
+  end
+
   defp get_tag(opts) do
-    timestamp = DateTime.utc_now() |> Calendar.strftime("%Y%m%d-%H%S")
+    timestamp = DateTime.utc_now() |> Calendar.strftime("%H%M%S")
 
     case Keyword.get(opts, :tag, nil) do
       nil -> timestamp
