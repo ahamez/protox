d8774826fce79f460fe0aa3188f92146a3de0642
Alexandre Hamez <alexandre.hamez@gmail.com>
2025-01-27 16:40:46 +0100

    refactor: unrol varint decoding
    Benchmark show a small speedup ranging from 1.02x to 1.08x

--- lib/protox/varint.ex
+++ lib/protox/varint.ex
@@ -38,6 +38,51 @@ defmodule Protox.Varint do
   def encode(v), do: <<1::1, v::7, encode(v >>> 7)::binary>>
 
   @spec decode(binary) :: {non_neg_integer, binary}
+  def decode(<<0::1, byte0::7, rest::binary>>),
+    do: {byte0, rest}
+
+  def decode(<<1::1, byte1::7, 0::1, byte0::7, rest::binary>>),
+    do: {byte1 <<< 0 ||| byte0 <<< 7, rest}
+
+  def decode(<<1::1, byte2::7, 1::1, byte1::7, 0::1, byte0::7, rest::binary>>),
+    do: {byte2 <<< 0 ||| byte1 <<< 7 ||| byte0 <<< 14, rest}
+
+  def decode(<<1::1, byte3::7, 1::1, byte2::7, 1::1, byte1::7, 0::1, byte0::7, rest::binary>>),
+    do: {byte3 <<< 0 ||| byte2 <<< 7 ||| byte1 <<< 14 ||| byte0 <<< 21, rest}
+
+  def decode(
+        <<1::1, byte4::7, 1::1, byte3::7, 1::1, byte2::7, 1::1, byte1::7, 0::1, byte0::7,
+          rest::binary>>
+      ),
+      do: {byte4 <<< 0 ||| byte3 <<< 7 ||| byte2 <<< 14 ||| byte1 <<< 21 ||| byte0 <<< 28, rest}
+
+  def decode(
+        <<1::1, byte5::7, 1::1, byte4::7, 1::1, byte3::7, 1::1, byte2::7, 1::1, byte1::7, 0::1,
+          byte0::7, rest::binary>>
+      ),
+      do:
+        {byte5 <<< 0 ||| byte4 <<< 7 ||| byte3 <<< 14 ||| byte2 <<< 21 ||| byte1 <<< 28 |||
+           byte0 <<< 35, rest}
+
+  def decode(
+        <<1::1, byte6::7, 1::1, byte5::7, 1::1, byte4::7, 1::1, byte3::7, 1::1, byte2::7, 1::1,
+          byte1::7, 0::1, byte0::7, rest::binary>>
+      ),
+      do:
+        {byte6 <<< 0 ||| byte5 <<< 7 ||| byte4 <<< 14 ||| byte3 <<< 21 ||| byte2 <<< 28 |||
+           byte1 <<< 35 |||
+           byte0 <<< 42, rest}
+
+  def decode(
+        <<1::1, byte7::7, 1::1, byte6::7, 1::1, byte5::7, 1::1, byte4::7, 1::1, byte3::7, 1::1,
+          byte2::7, 1::1, byte1::7, 0::1, byte0::7, rest::binary>>
+      ),
+      do:
+        {byte7 <<< 0 ||| byte6 <<< 7 ||| byte5 <<< 14 ||| byte4 <<< 21 ||| byte3 <<< 28 |||
+           byte2 <<< 35 |||
+           byte1 <<< 42 |||
+           byte0 <<< 49, rest}
+
   def decode(b), do: do_decode(0, 0, b)
 
   # -- Private
