8587266607bc0148fbc81eebe42c03e059e40d38
Alexandre Hamez <alexandre.hamez@gmail.com>
2025-02-03 13:40:05 +0100

    refactor!: encoding functions now also return the iodata size
    Also, prepend encoded fields to the accumulator.
    
    Benchmark shows a speedup ranging from ~15% to ~40%, at the cost of an increase of memory consumption ranging from ~17% to ~40%.
    However, on pathological cases of very large messages, there can can a decrease of speed of ~20%.

--- test/support/random_init.ex
+++ test/support/random_init.ex
@@ -13,7 +13,7 @@ defmodule Protox.RandomInit do
         generate_struct(mod, fields)
       end
 
-    {:ok, msg} = :proper_gen.pick(gen)
+    {:ok, msg} = :proper_gen.pick(gen, 5)
 
     msg
   end
