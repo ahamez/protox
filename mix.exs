06c91fb7dc8ca50e3a3038236f6023e35af78da1
Alexandre Hamez <alexandre.hamez@konboi.one>
2026-04-15 11:35:31 +0200

    chore: v2.0.8

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Protox.Mixfile do
   def project() do
     [
       app: :protox,
-      version: "2.0.7",
+      version: "2.0.8",
       elixir: "~> 1.15",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
