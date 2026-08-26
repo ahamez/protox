4b3effd1c236c8aa0e07c4d5d03edd0161885349
Alexandre Hamez <alexandre.hamez@konboi.one>
2026-08-26 17:22:13 +0200

    chore: drop no-op <<< 0 shifts in Varint.decode
    An @compile inline pragma was also evaluated and rejected: decode/1 is only
    called cross-module, where inlining cannot apply (benchee: reductions
    byte-identical).

--- lib/protox/varint.ex
+++ lib/protox/varint.ex
@@ -37,21 +37,21 @@ defmodule Protox.Varint do
   @spec decode(binary) :: {non_neg_integer, binary}
   def decode(<<0::1, byte0::7, rest::binary>>), do: {byte0, rest}
 
-  def decode(<<1::1, byte1::7, 0::1, byte0::7, rest::binary>>), do: {byte1 <<< 0 ||| byte0 <<< 7, rest}
+  def decode(<<1::1, byte1::7, 0::1, byte0::7, rest::binary>>), do: {byte1 ||| byte0 <<< 7, rest}
 
   def decode(<<1::1, byte2::7, 1::1, byte1::7, 0::1, byte0::7, rest::binary>>),
-    do: {byte2 <<< 0 ||| byte1 <<< 7 ||| byte0 <<< 14, rest}
+    do: {byte2 ||| byte1 <<< 7 ||| byte0 <<< 14, rest}
 
   def decode(<<1::1, byte3::7, 1::1, byte2::7, 1::1, byte1::7, 0::1, byte0::7, rest::binary>>),
-    do: {byte3 <<< 0 ||| byte2 <<< 7 ||| byte1 <<< 14 ||| byte0 <<< 21, rest}
+    do: {byte3 ||| byte2 <<< 7 ||| byte1 <<< 14 ||| byte0 <<< 21, rest}
 
   def decode(<<1::1, byte4::7, 1::1, byte3::7, 1::1, byte2::7, 1::1, byte1::7, 0::1, byte0::7, rest::binary>>),
-    do: {byte4 <<< 0 ||| byte3 <<< 7 ||| byte2 <<< 14 ||| byte1 <<< 21 ||| byte0 <<< 28, rest}
+    do: {byte4 ||| byte3 <<< 7 ||| byte2 <<< 14 ||| byte1 <<< 21 ||| byte0 <<< 28, rest}
 
   def decode(
         <<1::1, byte5::7, 1::1, byte4::7, 1::1, byte3::7, 1::1, byte2::7, 1::1, byte1::7, 0::1, byte0::7, rest::binary>>
       ) do
-    {byte5 <<< 0 ||| byte4 <<< 7 ||| byte3 <<< 14 ||| byte2 <<< 21 ||| byte1 <<< 28 ||| byte0 <<< 35, rest}
+    {byte5 ||| byte4 <<< 7 ||| byte3 <<< 14 ||| byte2 <<< 21 ||| byte1 <<< 28 ||| byte0 <<< 35, rest}
   end
 
   def decode(
@@ -59,16 +59,16 @@ defmodule Protox.Varint do
           byte0::7, rest::binary>>
       ),
       do:
-        {byte6 <<< 0 ||| byte5 <<< 7 ||| byte4 <<< 14 ||| byte3 <<< 21 ||| byte2 <<< 28 ||| byte1 <<< 35 |||
-           byte0 <<< 42, rest}
+        {byte6 ||| byte5 <<< 7 ||| byte4 <<< 14 ||| byte3 <<< 21 ||| byte2 <<< 28 ||| byte1 <<< 35 ||| byte0 <<< 42,
+         rest}
 
   def decode(
         <<1::1, byte7::7, 1::1, byte6::7, 1::1, byte5::7, 1::1, byte4::7, 1::1, byte3::7, 1::1, byte2::7, 1::1,
           byte1::7, 0::1, byte0::7, rest::binary>>
       ),
       do:
-        {byte7 <<< 0 ||| byte6 <<< 7 ||| byte5 <<< 14 ||| byte4 <<< 21 ||| byte3 <<< 28 ||| byte2 <<< 35 |||
-           byte1 <<< 42 ||| byte0 <<< 49, rest}
+        {byte7 ||| byte6 <<< 7 ||| byte5 <<< 14 ||| byte4 <<< 21 ||| byte3 <<< 28 ||| byte2 <<< 35 ||| byte1 <<< 42 |||
+           byte0 <<< 49, rest}
 
   def decode(b), do: do_decode(0, 0, b)
 
